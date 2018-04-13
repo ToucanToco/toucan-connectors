03ad4dcfe21e9dccc092b119dfbf7206cc85fbd7
PrettyWood <eric.jolibois@toucantoco.com>
2018-04-13 12:40:40 +0200

    fix imports

--- toucan_connectors/__init__.py
+++ toucan_connectors/__init__.py
@@ -1,1 +1,12 @@
+from contextlib import suppress
+
+with suppress(ImportError):
+    from .mongo.mongo_connector import MongoConnector
+with suppress(ImportError):
+    from .mssql.mssql_connector import MSSQLConnector
+with suppress(ImportError):
+    from .mysql.mysql_connector import MySQLConnector
+with suppress(ImportError):
+    from .postgres.postgresql_connector import PostgresConnector
+
 from .toucan_connector import ToucanDataSource, ToucanConnector
