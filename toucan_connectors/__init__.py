b483dae292fdf8a1f2c6bbd7ddbf961ee7423e5c
PrettyWood <eric.jolibois@toucantoco.com>
2018-04-16 11:22:46 +0200

    Update __init__.py

--- toucan_connectors/__init__.py
+++ toucan_connectors/__init__.py
@@ -1,16 +1,16 @@
 from contextlib import suppress
 
 with suppress(ImportError):
+    from .microstrategy.microstrategy_connector import MicroStrategyConnector
+with suppress(ImportError):
     from .mongo.mongo_connector import MongoConnector
 with suppress(ImportError):
     from .mssql.mssql_connector import MSSQLConnector
 with suppress(ImportError):
     from .mysql.mysql_connector import MySQLConnector
 with suppress(ImportError):
-    from .snowflake.snowflake_connector import SnowflakeConnector
-with suppress(ImportError):
     from .postgres.postgresql_connector import PostgresConnector
 with suppress(ImportError):
-    from .microstrategy.microstrategy_connector import MicroStrategyConnector
+    from .snowflake.snowflake_connector import SnowflakeConnector
 
 from .toucan_connector import ToucanDataSource, ToucanConnector
