8d7b285095314f5a7f41b15d9839d88a668f3def
Your Name <virginie.robidou@gmail.com>
2018-08-08 10:55:54 +0200

    new version

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ classifiers = [
 ]
 
 setup(name='toucan_connectors',
-      version='0.0.17',
+      version='0.1.0',
       description='Toucan Toco Connectors',
       author='Toucan Toco',
       author_email='dev@toucantoco.com',
