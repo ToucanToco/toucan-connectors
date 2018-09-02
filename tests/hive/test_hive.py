bed522ff6e07e41bd1897f748a45ac30acbd1680
piotch <pierre.schmidt@gmail.com>
2018-09-02 23:45:56 +0200

    Tests

--- tests/hive/test_hive.py
+++ tests/hive/test_hive.py
@@ -1,8 +1,38 @@
+import os
+from urllib.parse import urlparse
+
+from pyhive import hive
+import pytest
+
 from toucan_connectors.hive.hive_connector import HiveConnector, HiveDataSource
 
+if 'DOCKER_HOST' in os.environ:
+    HOST = urlparse(os.environ['DOCKER_HOST']).hostname
+else:
+    HOST = 'localhost'
 
-def test_get_df():
-    hc = HiveConnector(host='192.168.64.5', username='root', name='HiveTest')
-    hds = HiveDataSource(domain='test', name='HiveTest', query='select * from test')
-    df = hc.get_df(hds)
-    assert df['test.a'].iloc[0] == 12
+
+@pytest.fixture(scope='module')
+def hive_server(service_container):
+    def check_and_feed(host_port):
+        cursor = hive.connect(HOST, port=host_port, username='root').cursor()
+        cursor.execute('create table tests (a int)')
+        cursor.execute('insert into tests (a) values (12)')
+        cursor.close()
+
+    return service_container('hive', check_and_feed, timeout=600)
+
+
+@pytest.fixture
+def hive_connector(hive_server):
+    return HiveConnector(name='mycon', host=HOST, port=hive_server['port'], username='root')
+
+
+@pytest.fixture
+def hive_datasource():
+    return HiveDataSource(name='mycon', domain='mydomain', query='select * from tests')
+
+
+def test_get_df(hive_connector, hive_datasource):
+    df = hive_connector.get_df(hive_datasource)
+    assert df['tests.a'].iloc[0] == 12
