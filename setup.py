cb56b9c3f3ef22a6bd0a5641680a193ebf65fa99
Pierre <pierre.schmidt@gmail.com>
2018-09-26 16:07:25 +0200

    fix __init__.py (#35)
    * fix __init__.py
    
    * bump version

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ classifiers = [
 ]
 
 setup(name='toucan_connectors',
-      version='0.5.2',
+      version='0.5.3',
       description='Toucan Toco Connectors',
       author='Toucan Toco',
       author_email='dev@toucantoco.com',
