b0316b5164bf8397c1e824fd1ef1e20dea0964c0
piotch <pierre.schmidt@gmail.com>
2018-11-26 18:32:35 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ classifiers = [
 ]
 
 setup(name='toucan_connectors',
-      version='0.9.3',
+      version='0.10.0',
       description='Toucan Toco Connectors',
       author='Toucan Toco',
       author_email='dev@toucantoco.com',
