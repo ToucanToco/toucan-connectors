b2978b11b3f4d25200521b811396962360006eff
piotch <pierre.schmidt@gmail.com>
2018-11-26 18:30:05 +0100

    Missing test case

--- tests/test_common.py
+++ tests/test_common.py
@@ -48,8 +48,18 @@ def test_custom_token_server():
 
     responses.add(responses.POST, 'https://example.com', json={'token': 'a'})
 
+    # dummy request class just to introspect headers
     class TMP:
         headers = {}
 
     session.auth(TMP())
     assert TMP.headers['Authorization'] == 'Bearer a'
+
+    # custom_token_server with its own auth class
+    responses.add(responses.POST, 'https://example.com', json={'token': 'a'})
+    session = Auth(type='custom_token_server',
+                   args=['POST', 'https://example.com'],
+                   kwargs={'auth': {'type': 'basic', 'args': ['u', 'p']}}).get_session()
+
+    session.auth(TMP())
+    assert responses.calls[1].request.headers['Authorization'].startswith('Basic')
