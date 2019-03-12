6a5010ca6e1e17deba0954750691806ce65e1721
Frédéric Matigot <fred@fspot.org>
2019-03-12 09:17:36 +0100

    Allow validation field (#67)
    * allow validation field
    
    * comment unavailable docker image
    
    * test with another oracle image
    
    * test another image
    
    * oracle :: specify shm_size
    
    * -_-

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ classifiers = [
 ]
 
 setup(name='toucan_connectors',
-      version='0.10.4',
+      version='0.10.5',
       description='Toucan Toco Connectors',
       author='Toucan Toco',
       author_email='dev@toucantoco.com',
