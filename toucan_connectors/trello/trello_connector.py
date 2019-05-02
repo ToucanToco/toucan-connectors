e042dbcbfd8f754e04572e221452d411fd2b3eac
raphael <raphael.huille@toucantoco.com>
2019-05-02 17:21:51 +0200

    add the possibility to choose the requested fiedls

--- toucan_connectors/trello/trello_connector.py
+++ toucan_connectors/trello/trello_connector.py
@@ -5,18 +5,33 @@ Check https://developers.trello.com/docs/get-started for the official API docume
 
 The connector allow you to list in a dataset all the cards of a board with a chosen set of "fields".
 The available fields are listed in the `Field ` object.
+
+The dataset has one line by card and one columns by fields
 """
 
 import pandas as pd
-# from typing import List
+from typing import List
+from enum import Enum
 import requests
 import warnings
 
 from toucan_connectors.toucan_connector import ToucanConnector, ToucanDataSource
 
 
+class Fields(str, Enum):
+    id = 'id'
+    name = 'name'
+    url = 'url'
+    lists = 'lists'
+    members = 'members'
+    labels = 'labels'
+
+
 class TrelloDataSource(ToucanDataSource):
     board_id: str
+    fields_list: List[Fields] = [Fields.id, Fields.name, Fields.url,
+                                 Fields.lists, Fields.members,  Fields.labels]
+    custom_fields: bool = True
 
 
 class TrelloConnector(ToucanConnector):
@@ -33,7 +48,8 @@ class TrelloConnector(ToucanConnector):
             'customFieldsItem': True, **customParams
         }).json()
 
-    def replace_id_by_value(self, card_with_id, lists, labels, members, custom_fields):
+    def replace_id_by_value(self, card_with_id, lists=None, labels=None,
+                            members=None, custom_fields=None):
         """
         `card_with_id` is a dictionary containing all data of a card,
         but with unreadlable id instead of value
@@ -52,48 +68,64 @@ class TrelloConnector(ToucanConnector):
         card_with_value['url'] = card_with_id['url']
 
         # need to translate from a id to a value
-        card_with_value['lists'] = lists[card_with_id['idList']]
-        card_with_value['members'] = [members[member] for member in card_with_id['idMembers']]
-        card_with_value['labels'] = [labels[label['id']] for label in card_with_id['labels']]
+        if lists:
+            card_with_value['lists'] = lists[card_with_id['idList']]
+        if members:
+            card_with_value['members'] = [members[member] for member in card_with_id['idMembers']]
+        if labels:
+            card_with_value['labels'] = [labels[label['id']] for label in card_with_id['labels']]
 
-        for card_custom_field in card_with_id['customFieldItems']:
-            custom_field = custom_fields[card_custom_field['idCustomField']]
-            if custom_field['type'] == 'number':
-                card_with_value[custom_field['name']] = float(card_custom_field['value']['number'])
-            elif custom_field['type'] == 'text':
-                card_with_value[custom_field['name']] = card_custom_field['value']['text']
-            elif custom_field['type'] == 'date':
-                card_with_value[custom_field['name']] = card_custom_field['value']['date']
-            elif custom_field['type'] == 'checkbox':
-                if card_custom_field['value']['checked'] == 'true':
-                    card_with_value[custom_field['name']] = True
-            elif custom_field['type'] == 'list':
-                list_of_options = {x["id"]: x for x in custom_field['options']}
-                option = list_of_options[card_custom_field['idValue']]
-                card_with_value[custom_field['name']] = option['value']['text']
-            else:
-                warnings.warn(f"""The custom field {custom_field['name']} used into \
-                your board is of type {custom_field['type']}.
-                This type is not handled by this app and is simply ignored.""")
+        if custom_fields:
+            for card_custom_field in card_with_id['customFieldItems']:
+                custom_field = custom_fields[card_custom_field['idCustomField']]
+                if custom_field['type'] == 'number':
+                    card_with_value[custom_field['name']] = float(
+                        card_custom_field['value']['number'])
+                elif custom_field['type'] == 'text':
+                    card_with_value[custom_field['name']] = card_custom_field['value']['text']
+                elif custom_field['type'] == 'date':
+                    card_with_value[custom_field['name']] = card_custom_field['value']['date']
+                elif custom_field['type'] == 'checkbox':
+                    if card_custom_field['value']['checked'] == 'true':
+                        card_with_value[custom_field['name']] = True
+                elif custom_field['type'] == 'list':
+                    list_of_options = {x["id"]: x for x in custom_field['options']}
+                    option = list_of_options[card_custom_field['idValue']]
+                    card_with_value[custom_field['name']] = option['value']['text']
+                else:
+                    warnings.warn(f"""The custom field {custom_field['name']} used into \
+                    your board is of type {custom_field['type']}.
+                    This type is not handled by this app and is simply ignored.""")
 
         return card_with_value
 
     def get_df(self, data_source: TrelloDataSource) -> pd.DataFrame:
         # get board caracteristics
-        # the following dictionaries are
-        lists = {x['id']: x['name']
-                 for x in self.get(f'{data_source.board_id}/lists', fields='name')}
-        labels = {x['id']: x['name']
-                  for x in self.get(f'{data_source.board_id}/labels', fields='name')}
-        members = {x['id']: x['fullName']
-                   for x in self.get(f'{data_source.board_id}/members', fields='fullName')}
-        custom_fields = {x['id']: x
-                         for x in self.get(f'{data_source.board_id}/customFields', fields='name')}
+        # the following dictionaries are of the forme:
+        # id of values of fields as key and readable value as values
+        lists, labels,  members, custom_fields = None, None, None, None
+
+        if 'lists' in data_source.fields_list:
+            lists = {x['id']: x['name']
+                     for x in self.get(f'{data_source.board_id}/lists', fields='name')}
+        if 'labels' in data_source.fields_list:
+            labels = {x['id']: x['name']
+                      for x in self.get(f'{data_source.board_id}/labels', fields='name')}
+        if 'members' in data_source.fields_list:
+            members = {x['id']: x['fullName']
+                       for x in self.get(f'{data_source.board_id}/members', fields='fullName')}
+
+        if data_source.custom_fields:
+            custom_fields = {x['id']: x for x in self.get(
+                f'{data_source.board_id}/customFields', fields='name')}
 
         # get cards
         cards_with_id = self.get(f'{data_source.board_id}/cards',
-                                 fields=['name', 'idList', 'idMembers', 'labels', 'url'],
-                                 customFieldItems='true')
+                                 fields=['name', 'url']
+                                 + ['idList']*('lists' in data_source.fields_list)
+                                 + ['idMembers']*('members' in data_source.fields_list)
+                                 + ['labels']*('labels' in data_source.fields_list),
+                                 customFieldItems='true' if data_source.custom_fields else 'false')
 
         # replace all id in `cards_with_id` by the corresponding readable value
         cards_with_value = [self.replace_id_by_value(card_with_id, lists,
