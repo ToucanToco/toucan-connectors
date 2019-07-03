70f7779a8969725f2989a7a87706d4ed6973d5a5
Pierre <pierre.schmidt@toucantoco.com>
2019-07-03 07:39:57 +0200

    Update version

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ classifiers = [
 ]
 
 setup(name='toucan_connectors',
-      version='0.19.0',
+      version='0.20.0',
       description='Toucan Toco Connectors',
       author='Toucan Toco',
       author_email='dev@toucantoco.com',
