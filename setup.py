e115d87dd0207a0490ed9e7ed57ebe8cf6b56121
Virginie Robidou <virginie.robidou@gmail.com>
2019-07-12 23:33:04 +0200

    bump version

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ classifiers = [
 ]
 
 setup(name='toucan_connectors',
-      version='0.21.1',
+      version='0.22.0',
       description='Toucan Toco Connectors',
       author='Toucan Toco',
       author_email='dev@toucantoco.com',
