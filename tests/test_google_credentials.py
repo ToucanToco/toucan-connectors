b3c4f6835b961aa17e32fdb60cb2c911c18eeb6f
PrettyWood <em.jolibois@gmail.com>
2019-08-04 02:03:15 +0200

    google credentials :: ensure line breaks in private key are unescaped

---
+++ tests/test_google_credentials.py
@@ -0,0 +1,21 @@
+from toucan_connectors.google_credentials import GoogleCredentials
+
+
+def test_unespace_break_lines():
+    conf = {
+      "type": "service_account",
+      "project_id": "my_project_id",
+      "private_key_id": "my_private_key_id",
+      "private_key": "-----BEGIN PRIVATE KEY-----\\naaa\\nbbb\\n-----END PRIVATE KEY-----\\n",
+      "client_email": "my_client_email",
+      "client_id": "my_client_id",
+      "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+      "token_uri": "https://oauth2.googleapis.com/token",
+      "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+      "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/xxx.iam.gserviceaccount.com"  # noqa: E501
+    }
+    credentials = GoogleCredentials(**conf)
+    assert credentials.private_key == '-----BEGIN PRIVATE KEY-----\n' \
+                                      'aaa\n' \
+                                      'bbb\n' \
+                                      '-----END PRIVATE KEY-----\n'
