627e9dbc9750bbabce3b5c56d974eed4a9eb968a
PrettyWood <em.jolibois@gmail.com>
2019-09-18 13:58:05 +0200

    Bump to v0.24.0
    - Add cache on validate methods for MongoConnector
    - MongoConnector can now be used as a context manager
    - HttpAPIConnector now handles SSL certificates

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ classifiers = [
 ]
 
 setup(name='toucan_connectors',
-      version='0.23.4',
+      version='0.24.0',
       description='Toucan Toco Connectors',
       author='Toucan Toco',
       author_email='dev@toucantoco.com',
