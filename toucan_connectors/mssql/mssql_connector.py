e07bf1b18c6f9aecc496d26ba0ccee2f25048976
vincent_d <vincent.destrait@toucantoco.com>
2019-09-24 17:52:00 +0200

    [schema] improve mssql_connector

--- toucan_connectors/mssql/mssql_connector.py
+++ toucan_connectors/mssql/mssql_connector.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import pymssql
-from pydantic import constr
+from pydantic import Schema, SecretStr, constr
 
 from toucan_connectors.toucan_connector import ToucanConnector, ToucanDataSource
 
@@ -8,8 +8,14 @@ from toucan_connectors.toucan_connector import ToucanConnector, ToucanDataSource
 class MSSQLDataSource(ToucanDataSource):
     # By default SQL Server selects the database which is set
     # as default for specific user
-    database: str = None
-    query: constr(min_length=1)
+    database: str = Schema(
+        None,
+        description='The name of the database you want to query. '
+        "By default SQL Server selects the user's default database",
+    )
+    query: constr(min_length=1) = Schema(
+        ..., description='You can write your SQL query here', widget='sql'
+    )
 
 
 class MSSQLConnector(ToucanConnector):
@@ -19,18 +25,28 @@ class MSSQLConnector(ToucanConnector):
 
     data_source_model: MSSQLDataSource
 
-    host: str
-    user: str
-    password: str = None
-    port: int = None
-    connect_timeout: int = None
+    host: str = Schema(
+        ...,
+        description='The domain name (preferred option as more dynamic) or '
+        'the hardcoded IP address of your database server',
+    )
+
+    port: int = Schema(None, description='The listening port of your database server')
+    user: str = Schema(..., description='Your login username')
+    password: SecretStr = Schema(None, description='Your login password')
+    connect_timeout: int = Schema(
+        None,
+        title='Connection timeout',
+        description='You can set a connection timeout in seconds here, i.e. the maximum length '
+        'of time you want to wait for the server to respond. None by default',
+    )
 
     def get_connection_params(self, database):
         con_params = {
             'server': self.host,
             'user': self.user,
             'database': database,
-            'password': self.password,
+            'password': self.password.get_secret_value() if self.password else None,
             'port': self.port,
             'login_timeout': self.connect_timeout,
             'as_dict': True,
