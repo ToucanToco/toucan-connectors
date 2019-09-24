57dd55adca4ae2c389d5224c41c5ebe506001400
vincent_d <vincent.destrait@toucantoco.com>
2019-09-24 17:22:41 +0200

    [schema] improve google_spreadsheet_connector

--- toucan_connectors/google_spreadsheet/google_spreadsheet_connector.py
+++ toucan_connectors/google_spreadsheet/google_spreadsheet_connector.py
@@ -3,15 +3,27 @@ from typing import List
 import gspread
 import pandas as pd
 from oauth2client.service_account import ServiceAccountCredentials
+from pydantic import Schema
 
 from toucan_connectors.google_credentials import GoogleCredentials
 from toucan_connectors.toucan_connector import ToucanConnector, ToucanDataSource
 
 
 class GoogleSpreadsheetDataSource(ToucanDataSource):
-    spreadsheet_id: str
-    sheetname: str = None
-    skip_rows: int = 0
+    spreadsheet_id: str = Schema(
+        ...,
+        title='Spreadsheet ID',
+        description='You can find this ID in the URL of your spreadsheet, '
+        'just after the base path "https://docs.google.com/spreadsheets/d/"',
+    )
+    sheetname: str = Schema(
+        None, description='If not specified, the first sheet will be extracted by default'
+    )
+    skip_rows: int = Schema(
+        0,
+        title='Number of rows to skip',
+        description='If the first rows of your spreadsheet do not contain relevant data',
+    )
 
 
 class GoogleSpreadsheetConnector(ToucanConnector):
@@ -23,12 +35,26 @@ class GoogleSpreadsheetConnector(ToucanConnector):
 
     data_source_model: GoogleSpreadsheetDataSource
 
-    credentials: GoogleCredentials
-    scope: List[str] = [
-        'https://www.googleapis.com/auth/drive',
-        'https://www.googleapis.com/auth/spreadsheets',
-        'https://spreadsheets.google.com/feeds',
-    ]
+    credentials: GoogleCredentials = Schema(
+        ...,
+        title='Google Credentials',
+        description='For authentication, download an authentication file from your '
+        '<a href="https://console.developers.google.com/apis/credentials">Google Console</a> '
+        'and use the values here. This is an oauth2 credential file. For more information see this '
+        '<a href="https://gspread.readthedocs.io/en/latest/oauth2.html">documentation</a>. '
+        'You should use "service_account" credentials, which is the preferred type of credentials '
+        'to use when authenticating on behalf of a service or application',
+    )
+    scope: List[str] = Schema(
+        [
+            'https://www.googleapis.com/auth/drive',
+            'https://www.googleapis.com/auth/spreadsheets',
+            'https://spreadsheets.google.com/feeds',
+        ],
+        description='OAuth 2.0 scopes define the level of access you need to '
+        'request the Google APIs. For more information, see this '
+        '<a href="https://developers.google.com/identity/protocols/googlescopes">documentation</a>',
+    )
 
     def _retrieve_data(self, data_source):
         credentials = ServiceAccountCredentials.from_json_keyfile_dict(
