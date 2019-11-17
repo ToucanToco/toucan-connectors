f8c3bb896dc3ebb9bfceb582487b908b74a49b83
piotch <pierre.schmidt@gmail.com>
2019-11-17 14:26:44 +0000

    v0.27.1
    - Fix a bug with snowflake connector missing a USE WAREHOUSE
    instruction.

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ classifiers = [
 
 setup(
     name='toucan_connectors',
-    version='0.27.0',
+    version='0.27.1',
     description='Toucan Toco Connectors',
     author='Toucan Toco',
     author_email='dev@toucantoco.com',
