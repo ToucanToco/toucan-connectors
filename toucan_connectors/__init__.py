0064e8beff5d9711cae69cb488ea697814329795
PrettyWood <em.jolibois@gmail.com>
2020-01-14 09:55:52 +0100

    [connector registry] add logos and all connector details in a unique object

--- toucan_connectors/__init__.py
+++ toucan_connectors/__init__.py
@@ -1,39 +1,122 @@
+import base64
+from contextlib import suppress
 from importlib import import_module
+from pathlib import Path
 
 from .toucan_connector import DataSlice, ToucanConnector, ToucanDataSource
 
-CONNECTORS_CATALOGUE = {
-    'AdobeAnalytics': 'adobe_analytics.adobe_analytics_connector.AdobeAnalyticsConnector',
-    'AzureMSSQL': 'azure_mssql.azure_mssql_connector.AzureMSSQLConnector',
-    'Dataiku': 'dataiku.dataiku_connector.DataikuConnector',
-    'elasticsearch': 'elasticsearch.elasticsearch_connector.ElasticsearchConnector',
-    'facebook_insights': 'facebook_insights.facebook_insights_connector.FacebookInsightsConnector',
-    'GoogleAnalytics': 'google_analytics.google_analytics_connector.GoogleAnalyticsConnector',
-    'GoogleBigQuery': 'google_big_query.google_big_query_connector.GoogleBigQueryConnector',
-    'GoogleCloudMySQL': 'google_cloud_mysql.google_cloud_mysql_connector.GoogleCloudMySQLConnector',
-    'google_my_business': 'google_my_business.google_my_business_connector.GoogleMyBusinessConnector',
-    'GoogleSheets': 'google_sheets.google_sheets_connector.GoogleSheetsConnector',
-    'GoogleSpreadsheet': 'google_spreadsheet.google_spreadsheet_connector.GoogleSpreadsheetConnector',
-    'Hive': 'hive.hive_connector.HiveConnector',
-    'HttpAPI': 'http_api.http_api_connector.HttpAPIConnector',
-    'MicroStrategy': 'micro_strategy.micro_strategy_connector.MicroStrategyConnector',
-    'MongoDB': 'mongo.mongo_connector.MongoConnector',
-    'MSSQL': 'mssql.mssql_connector.MSSQLConnector',
-    'MySQL': 'mysql.mysql_connector.MySQLConnector',
-    'OData': 'odata.odata_connector.ODataConnector',
-    'OracleSQL': 'oracle_sql.oracle_sql_connector.OracleSQLConnector',
-    'Postgres': 'postgres.postgresql_connector.PostgresConnector',
-    'ROK': 'rok.rok_connector.RokConnector',
-    'SapHana': 'sap_hana.sap_hana_connector.SapHanaConnector',
-    'Snowflake': 'snowflake.snowflake_connector.SnowflakeConnector',
-    'ToucanToco': 'toucan_toco.toucan_toco_connector.ToucanTocoConnector',
-    'Trello': 'trello.trello_connector.TrelloConnector',
-    'Wootric': 'wootric.wootric_connector.WootricConnector',
+CONNECTORS_REGISTRY = {
+    'AdobeAnalytics': {
+        'connector': 'adobe_analytics.adobe_analytics_connector.AdobeAnalyticsConnector',
+        'label': 'Adobe Analytics',
+        'logo': 'adobe_analytics/adobe-analytics.png',
+    },
+    'AzureMSSQL': {
+        'connector': 'azure_mssql.azure_mssql_connector.AzureMSSQLConnector',
+        'label': 'Microsoft Azure SQL',
+        'logo': 'azure_mssql/sql-azure.png',
+    },
+    'Dataiku': {
+        'connector': 'dataiku.dataiku_connector.DataikuConnector',
+        'logo': 'dataiku/dataiku.png',
+    },
+    'elasticsearch': {
+        'connector': 'elasticsearch.elasticsearch_connector.ElasticsearchConnector',
+        'label': 'Elasticsearch',
+        'logo': 'elasticsearch/elasticsearch.png',
+    },
+    'facebook_insights': {
+        'connector': 'facebook_insights.facebook_insights_connector.FacebookInsightsConnector',
+        'label': 'Facebook Insights',
+        'logo': 'facebook_insights/facebook-insights.png',
+    },
+    'GoogleAnalytics': {
+        'connector': 'google_analytics.google_analytics_connector.GoogleAnalyticsConnector',
+        'label': 'Google Analytics',
+        'logo': 'google_analytics/google-analytics.png',
+    },
+    'GoogleBigQuery': {
+        'connector': 'google_big_query.google_big_query_connector.GoogleBigQueryConnector',
+        'label': 'Google Big Query',
+        'logo': 'google_big_query/google-bigquery.png',
+    },
+    'GoogleCloudMySQL': {
+        'connector': 'google_cloud_mysql.google_cloud_mysql_connector.GoogleCloudMySQLConnector',
+        'label': 'Google Cloud MySQL',
+        'logo': 'google_cloud_mysql/google-cloud-mysql.png',
+    },
+    'google_my_business': {
+        'connector': 'google_my_business.google_my_business_connector.GoogleMyBusinessConnector',
+        'label': 'Google My Business',
+        'logo': 'google_my_business/google-my-business.png',
+    },
+    'GoogleSheets': {
+        'connector': 'google_sheets.google_sheets_connector.GoogleSheetsConnector',
+        'label': 'Google Sheets',
+        'logo': 'google_sheets/google-sheets.png',
+    },
+    'GoogleSpreadsheet': {
+        'connector': 'google_spreadsheet.google_spreadsheet_connector.GoogleSpreadsheetConnector',
+        'label': 'Google Spreadsheet',
+        'logo': 'google_spreadsheet/google-spreadsheet.png',
+    },
+    'Hive': {'connector': 'hive.hive_connector.HiveConnector', 'logo': 'hive/hive.png',},
+    'HttpAPI': {
+        'connector': 'http_api.http_api_connector.HttpAPIConnector',
+        'label': 'Http API',
+        'logo': 'http_api/http-api.png',
+    },
+    'MicroStrategy': {
+        'connector': 'micro_strategy.micro_strategy_connector.MicroStrategyConnector',
+        'logo': 'micro_strategy/microstrategy.png',
+    },
+    'MongoDB': {'connector': 'mongo.mongo_connector.MongoConnector', 'logo': 'mongo/mongo-db.png',},
+    'MSSQL': {
+        'connector': 'mssql.mssql_connector.MSSQLConnector',
+        'label': 'Microsoft SQL Server',
+        'logo': 'mssql/mssql.png',
+    },
+    'MySQL': {'connector': 'mysql.mysql_connector.MySQLConnector', 'logo': 'mysql/mysql.png',},
+    'OData': {'connector': 'odata.odata_connector.ODataConnector', 'logo': 'odata/odata.png',},
+    'OracleSQL': {
+        'connector': 'oracle_sql.oracle_sql_connector.OracleSQLConnector',
+        'label': 'Oracle SQL',
+        'logo': 'oracle_sql/oracle-sql.png',
+    },
+    'Postgres': {
+        'connector': 'postgres.postgresql_connector.PostgresConnector',
+        'label': 'PostgreSQL',
+        'logo': 'postgres/postgres.png',
+    },
+    'ROK': {'connector': 'rok.rok_connector.RokConnector', 'logo': 'rok/rok.png',},
+    'SapHana': {
+        'connector': 'sap_hana.sap_hana_connector.SapHanaConnector',
+        'label': 'SAP HANA',
+        'logo': 'sap_hana/sap-hana.png',
+    },
+    'Snowflake': {
+        'connector': 'snowflake.snowflake_connector.SnowflakeConnector',
+        'logo': 'snowflake/snowflake.jpeg',
+    },
+    'ToucanToco': {
+        'connector': 'toucan_toco.toucan_toco_connector.ToucanTocoConnector',
+        'label': 'Toucan Toco',
+        'logo': 'toucan_toco/toucan.png',
+    },
+    'Trello': {
+        'connector': 'trello.trello_connector.TrelloConnector',
+        'logo': 'trello/trello.png',
+    },
+    'Wootric': {
+        'connector': 'wootric.wootric_connector.WootricConnector',
+        'logo': 'wootric/wootric.png',
+    },
 }
-ALL_CONNECTOR_TYPES = list(CONNECTORS_CATALOGUE)
-AVAILABLE_CONNECTORS = {}
 
-for connector_type, connector_path in CONNECTORS_CATALOGUE.items():
+
+for connector_type, connector_infos in CONNECTORS_REGISTRY.items():
+    # Remove the path of the connector and set the connector class if available
+    connector_path = connector_infos.pop('connector')
     module_path, connector_cls_name = connector_path.rsplit('.', 1)
     try:
         mod = import_module(f'.{module_path}', 'toucan_connectors')
@@ -41,4 +124,15 @@ for connector_type, connector_path in CONNECTORS_CATALOGUE.items():
         pass
     else:
         connector_cls = getattr(mod, connector_cls_name)
-        AVAILABLE_CONNECTORS[connector_type] = connector_cls
+        connector_infos['connector'] = connector_cls
+        with suppress(AttributeError):
+            connector_infos['bearer_integration'] = connector_cls.bearer_integration
+
+    # Set default label if not set
+    if 'label' not in connector_infos:
+        connector_infos['label'] = connector_type
+
+    # Convert logo into base64
+    logo_relative_path = connector_infos.get('logo', 'default-logo.png')
+    with open(Path(__file__).parent / logo_relative_path, 'rb') as f:
+        connector_infos['logo'] = base64.b64encode(f.read()).decode('utf8')
