82ea0b2fa92088b488182da3b850bcec3b560dc3
PrettyWood <em.jolibois@gmail.com>
2020-01-14 15:26:25 +0100

    [lightspeed] Add new connector

---
+++ toucan_connectors/lightspeed/lightspeed_connector.py
@@ -0,0 +1,32 @@
+import pandas as pd
+from jq import jq
+from pydantic import Schema
+
+from toucan_connectors.common import FilterSchema, nosql_apply_parameters_to_query
+from toucan_connectors.toucan_connector import ToucanConnector, ToucanDataSource
+
+
+class LightspeedDataSource(ToucanDataSource):
+    endpoint: str = Schema(
+        ...,
+        title='Endpoint of the Lightspeed API',
+        description='See https://developers.lightspeedhq.com/retail/endpoints/Account/',
+    )
+    filter: str = FilterSchema
+
+
+class LightspeedConnector(ToucanConnector):
+    """
+    This is a connector for [Lightspeed](https://developers.lightspeedhq.com/retail/endpoints/Account/)
+    using [Bearer.sh](https://app.bearer.sh/)
+    """
+
+    data_source_model: LightspeedDataSource
+    bearer_integration = 'lightspeed'
+    bearer_auth_id: str
+
+    def _retrieve_data(self, data_source: LightspeedDataSource) -> pd.DataFrame:
+        endpoint = nosql_apply_parameters_to_query(data_source.endpoint, data_source.parameters)
+        data = self.bearer_oauth_get_endpoint(endpoint)
+        data = jq(data_source.filter).transform(data)
+        return pd.DataFrame(data)
