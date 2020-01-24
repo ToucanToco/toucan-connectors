f9eaf7b6f0ea03376dec2619fd6dc93f9c84a22a
PrettyWood <em.jolibois@gmail.com>
2020-01-24 14:40:21 +0100

    [pydantic 1] fix :: pydantic now uses __dict__ for attributes
    https://github.com/samuelcolvin/pydantic/pull/712

--- toucan_connectors/mongo/mongo_connector.py
+++ toucan_connectors/mongo/mongo_connector.py
@@ -137,14 +137,13 @@ class MongoConnector(ToucanConnector):
         return ok_checks + [new_check] + not_validated_checks
 
     def _get_mongo_client_kwargs(self):
-        mongo_client_kwargs = self.dict().copy()
-        for field in ToucanConnector.schema()['properties']:
-            mongo_client_kwargs.pop(field)
-        if mongo_client_kwargs['password'] is not None:
-            mongo_client_kwargs['password'] = mongo_client_kwargs['password'].get_secret_value()
+        # We don't want parent class attributes nor the `client` property
+        # nor attributes with `None` value
+        to_exclude = set(ToucanConnector.__fields__) | {'client'}
+        mongo_client_kwargs = self.dict(exclude=to_exclude, exclude_none=True).copy()
 
-        # prevent optional parameters to be sent to MongoClient as None
-        mongo_client_kwargs = {k: v for (k, v) in mongo_client_kwargs.items() if v is not None}
+        if 'password' in mongo_client_kwargs:
+            mongo_client_kwargs['password'] = mongo_client_kwargs['password'].get_secret_value()
 
         return mongo_client_kwargs
 
