0364584dc670625bce9052920bff9b8b19cb4792
PrettyWood <em.jolibois@gmail.com>
2020-03-18 10:43:56 +0100

    [version] 0.30.5

--- setup.py
+++ setup.py
@@ -64,7 +64,7 @@ def get_static_file_paths():
 
 setup(
     name='toucan_connectors',
-    version='0.30.4',
+    version='0.30.5',
     description='Toucan Toco Connectors',
     long_description=(HERE / 'README.md').read_text(encoding='utf-8'),
     long_description_content_type='text/markdown',
