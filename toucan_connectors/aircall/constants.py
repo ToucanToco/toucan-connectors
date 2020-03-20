1bfe2e445bd1b985e77b66a847d6d44bbe7c0d04
testinnplayin <archaurore@gmail.com>
2020-03-20 13:03:12 +0100

    added a dictionary linked to dataset

---
+++ toucan_connectors/aircall/constants.py
@@ -0,0 +1,27 @@
+COLUMN_DICTIONARY = {
+    'calls' : [
+        'id',
+        'direction',
+        'duration',
+        'answered_at',
+        'ended_at',
+        'raw_digits',
+        'user_id',
+        'tags',
+        'user_name',
+        'team',
+        'day'
+    ],
+    'tags' : [
+        'id',
+        'name',
+        'color',
+        'description'
+    ],
+    'users': [
+        'user_id',
+        'user_name',
+        'team',
+        'user_created_at'
+    ]
+}
