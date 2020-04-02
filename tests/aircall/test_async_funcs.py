6e488abd3916d9fac3e91962eff8df7df5f3d269
testinnplayin <archaurore@gmail.com>
2020-04-02 09:24:23 +0200

    Added a new test for absence of meta in response
    - The real code was already added in a prior commit
    - The response is still returned but recursive function does not throw
    error

--- tests/aircall/test_async_funcs.py
+++ tests/aircall/test_async_funcs.py
@@ -27,6 +27,15 @@ async def send_next_link(req):
     return web.json_response(fake_response)
 
 
+async def send_no_meta(req):
+    fake_response = {
+        'data' : {
+            'stuff' : 'stuff'
+        }
+    }
+    return web.json_response(fake_response)
+
+
 async def test_fetch_page_with_no_next_page(aiohttp_client, loop):
     """Tests fetch_page to see if it stops when no next_page_link"""
     app = web.Application(loop=loop)
@@ -35,10 +44,8 @@ async def test_fetch_page_with_no_next_page(aiohttp_client, loop):
     # limit is 10 and run is 0 i.e. this is the first run and it should
     # cap out at 10 runs max
     res = await fetch_page('/foo', [], client, 10, 0)
-    assert type(res) == list
     assert len(res) == 1
     res_dict = res[0]
-    assert type(res_dict) == dict
     assert res_dict.get('data', None) is not None
     assert res_dict.get('meta', None) is not None
 
@@ -51,5 +58,14 @@ async def test_fetch_page_with_low_limit(aiohttp_client, loop):
     client = await aiohttp_client(app)
     # limit is only 1 and run is 0 i.e. this is the first run
     res = await fetch_page(endpoint, [], client, 1, 0)
-    assert type(res) == list
+    assert len(res) == 1
+
+
+async def test_fetch_page_with_no_meta(aiohttp_client, loop):
+    """Tests that no meta object in response is not an issue"""
+    app = web.Application(loop=loop)
+    endpoint = '/bar'
+    app.router.add_get(endpoint, send_no_meta)
+    client = await aiohttp_client(app)
+    res = await fetch_page(endpoint, [], client, 1, 0)
     assert len(res) == 1
