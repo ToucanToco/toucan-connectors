b3cec3bbaeb5817a9743da9da029c89e5961b9de
David Nowinsky <david@nowinsky.net>
2020-04-07 13:53:22 +0200

    Distinct patching of coroutines before/after 3.8

--- tests/aircall/helpers.py
+++ tests/aircall/helpers.py
@@ -1,5 +1,6 @@
 """Helpers functions for setting up tests for AirCall connector"""
 import asyncio
+import sys
 
 from tests.aircall.mock_results import fake_teams, fake_users
 from toucan_connectors.aircall.aircall_connector import AircallConnector, AircallDataSource
@@ -19,22 +20,32 @@ def build_mock_fetch_data(fake_data, mocker):
     """
     Builds a mock version of the fetch_data function
     """
-    f = asyncio.Future()
-    f.set_result(fake_data)
 
-    return mocker.patch('toucan_connectors.aircall.aircall_connector.fetch_page', return_value=f)
+    # In python > 3.8, patch detects we're mocking a coroutine and replace it by an AsyncMock
+    if sys.version_info > (3, 8):
+        return_value = fake_data
+    # In python < 3.8, patch only uses a MagicMock, which is not awaitable 
+    else:
+        f = asyncio.Future()
+        f.set_result(fake_data)
+        return_value = f
+
+    return mocker.patch('toucan_connectors.aircall.aircall_connector.fetch_page', return_value=return_value)
 
 
 def build_complex_mock_fetch_data(mocker):
     """
     Builds a mock version of the fetch_data function
     """
-
-    f_1 = asyncio.Future()
-    f_1.set_result(fake_teams)
-    f_2 = asyncio.Future()
-    f_2.set_result(fake_users)
+    if sys.version_info > (3, 8):
+        return_value = [fake_teams, fake_users]
+    else:
+        f_teams = asyncio.Future()
+        f_teams.set_result(fake_teams)
+        f_users = asyncio.Future()
+        f_users.set_result(fake_users)
+        return_value = [f_teams, f_users]
 
     return mocker.patch(
-        'toucan_connectors.aircall.aircall_connector.fetch_page', side_effect=[f_1, f_2]
+        'toucan_connectors.aircall.aircall_connector.fetch_page', side_effect=return_value
     )
