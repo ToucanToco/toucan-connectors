e25e531ee7af1384d065bd1f534a01605e9c076c
testinnplayin <archaurore@gmail.com>
2020-04-15 14:53:00 +0200

    Removed unnecessary helper
    - added FILTER_DICTIONARY to the connector
    - removed try catches for loop in hopes that it won't mess things up

--- toucan_connectors/aircall/aircall_connector.py
+++ toucan_connectors/aircall/aircall_connector.py
@@ -1,4 +1,3 @@
-# from dotenv import load_dotenv
 import asyncio
 import os
 from enum import Enum
@@ -12,17 +11,11 @@ from pydantic import Field
 from toucan_connectors.common import nosql_apply_parameters_to_query
 from toucan_connectors.toucan_connector import ToucanConnector, ToucanDataSource
 
-from .constants import MAX_RUNS, PER_PAGE
-from .helpers import build_df, build_empty_df, generate_multiple_jq_filters, generate_tags_filter
+from .constants import FILTER_DICTIONARY, MAX_RUNS, PER_PAGE
+from .helpers import build_df, build_empty_df
 
-# load_dotenv()
-
-# temporary constant that will be removed in production-ready code
-# STUFF = '156faf0053c34ea6535126f9274181f4:1434a05fe17fe0cd0121d840966d8d71@'
 BASE_ROUTE = f'https://proxy.bearer.sh/aircall_oauth'
-# BEARER_API_KEY = os.getenv('BEARER_API_KEY')
 BEARER_API_KEY = os.environ.get('BEARER_API_KEY')
-# BEARER_AUTH_ID = os.getenv('BEARER_AUTH_ID')
 
 
 async def fetch_page(
@@ -47,7 +40,6 @@ async def fetch_page(
 
     if next_page_link is not None and current_pass < limit:
         new_endpoint = next_page_link
-        # new_endpoint = new_endpoint.replace('//', f'//{STUFF}')
         data_list = await fetch_page(new_endpoint, data_list, session, limit, current_pass)
 
     return data_list
@@ -56,7 +48,6 @@ async def fetch_page(
 async def fetch(new_endpoint, session: ClientSession) -> dict:
     """The basic fetch function"""
     async with session.get(new_endpoint) as res:
-        # print('res ', res)
         return await res.json()
 
 
@@ -93,8 +84,9 @@ class AircallConnector(ToucanConnector):
                 fetch_page(teams_endpoint, [], session, limit, 0,),
                 fetch_page(variable_endpoint, [], session, limit, 0,),
             )
-            # print(team_data, variable_data)
-            team_jq_filter, variable_jq_filter = generate_multiple_jq_filters(dataset)
+
+            team_jq_filter = FILTER_DICTIONARY.get('teams')
+            variable_jq_filter = FILTER_DICTIONARY.get(dataset, 'users')
 
             team_data = pyjq.first(team_jq_filter, {'results': team_data})
             variable_data = pyjq.first(variable_jq_filter, {'results': variable_data})
@@ -106,27 +98,29 @@ class AircallConnector(ToucanConnector):
         headers = {'Authorization': BEARER_API_KEY, 'Bearer-Auth-Id': self.bearer_auth_id}
         async with ClientSession(headers=headers) as session:
             raw_data = await fetch_page(variable_endpoint, [], session, limit, 1,)
-            jq_filter = generate_tags_filter(dataset)
+            jq_filter = FILTER_DICTIONARY.get(dataset, 'tags')
 
             return pyjq.first(jq_filter, {'results': raw_data})
 
     def run_fetches(self, dataset, query, limit) -> Tuple[List[dict], List[dict]]:
         """sets up event loop and fetches for 'calls' and 'users' datasets"""
-        try:
-            loop = asyncio.get_event_loop()
-        except RuntimeError:
-            loop = asyncio.new_event_loop()
-            asyncio.set_event_loop(loop)
+        # try:
+        #     loop = asyncio.get_event_loop()
+        # except RuntimeError:
+        #     loop = asyncio.new_event_loop()
+        #     asyncio.set_event_loop(loop)
+        loop = asyncio.get_event_loop()
         future = asyncio.ensure_future(self._get_data(dataset, query, limit))
         return loop.run_until_complete(future)
 
     def run_fetches_for_tags(self, dataset, query, limit):
         """sets up event loop and fetches for 'tags' dataset"""
-        try:
-            loop = asyncio.get_event_loop()
-        except RuntimeError:
-            loop = asyncio.new_event_loop()
-            asyncio.set_event_loop(loop)
+        # try:
+        #     loop = asyncio.get_event_loop()
+        # except RuntimeError:
+        #     loop = asyncio.new_event_loop()
+        #     asyncio.set_event_loop(loop)
+        loop = asyncio.get_event_loop()
         future = asyncio.ensure_future(self._get_tags(dataset, query, limit))
         return loop.run_until_complete(future)
 
--- toucan_connectors/aircall/helpers.py
+++ toucan_connectors/aircall/helpers.py
@@ -4,7 +4,7 @@ from typing import List
 
 import pandas as pd
 
-from .constants import COLUMN_DICTIONARY, FILTER_DICTIONARY
+from .constants import COLUMN_DICTIONARY
 
 
 def resolve_calls_df(team_data, call_data) -> pd.DataFrame:
@@ -48,7 +48,7 @@ def build_df(dataset: str, list_of_data: List[dict]) -> pd.DataFrame:
         return (
             pd.concat(list_of_data, sort=False, ignore_index=True)
             .drop_duplicates(['user_id'], keep='first')
-            .assign(**{'user_created_at': lambda x: x['user_created_at'].str[:10]})
+            .assign(user_created_at=lambda x: x['user_created_at'].str[:10])
         )
     elif dataset == 'calls':
         empty_df, team_data, call_data = list_of_data
@@ -56,11 +56,9 @@ def build_df(dataset: str, list_of_data: List[dict]) -> pd.DataFrame:
         df = resolve_calls_df(team_data, call_data)
 
         total_df = pd.concat([empty_df, df], sort=False, ignore_index=True).assign(
-            **{
-                'answered_at': lambda t: pd.to_datetime(t['answered_at'], unit='s'),
-                'ended_at': lambda t: pd.to_datetime(t['ended_at'], unit='s'),
-                'day': lambda t: t['ended_at'].astype(str).str[:10],
-            }
+            answered_at=lambda t: pd.to_datetime(t['answered_at'], unit='s'),
+            ended_at=lambda t: pd.to_datetime(t['ended_at'], unit='s'),
+            day=lambda t: t['ended_at'].astype(str).str[:10],
         )
         return total_df[COLUMN_DICTIONARY[dataset]]
 
@@ -68,22 +66,3 @@ def build_df(dataset: str, list_of_data: List[dict]) -> pd.DataFrame:
 def build_empty_df(dataset: str) -> pd.DataFrame:
     """Provides column headers for empty dataframes"""
     return pd.DataFrame(columns=COLUMN_DICTIONARY[dataset])
-
-
-def generate_multiple_jq_filters(dataset: str) -> List[str]:
-    """
-    Provides two separate jq filters;
-    used in calls and users datasets
-    """
-
-    teams_jq_filter: str = FILTER_DICTIONARY['teams']
-
-    # NOTE: 'users' is the default dataset
-    variable_jq_filter: str = FILTER_DICTIONARY.get(dataset, FILTER_DICTIONARY['users'])
-
-    return [teams_jq_filter, variable_jq_filter]
-
-
-def generate_tags_filter(dataset: str) -> str:
-    """Provides a single, simple jq filter; only for tags call"""
-    return FILTER_DICTIONARY.get(dataset, 'tags')
