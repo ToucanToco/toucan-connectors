bad301d03dce541e9ff6aeb78c0b769fd8f1325f
testinnplayin <testinnplayin@users.noreply.github.com>
2020-06-22 18:32:20 +0200

    update version to v0.37.1 (#174)

--- setup.py
+++ setup.py
@@ -67,7 +67,7 @@ def get_static_file_paths():
 
 setup(
     name='toucan_connectors',
-    version='0.37.0',
+    version='0.37.1',
     description='Toucan Toco Connectors',
     long_description=(HERE / 'README.md').read_text(encoding='utf-8'),
     long_description_content_type='text/markdown',
