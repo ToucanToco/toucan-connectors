084ac2cec964ef46641dfbc656200e3b6494cb8e
Martin DONADIEU <martindonadieu@gmail.com>
2020-07-09 00:46:10 +0200

    Update documentdb_connector.py

--- toucan_connectors/documentdb/documentdb_connector.py
+++ toucan_connectors/documentdb/documentdb_connector.py
@@ -235,7 +235,7 @@ class DocumentDBConnector(ToucanConnector):
             if limit is not None:
                 data_source.query.append({'$limit': limit})   
             data_source_count.query.append(group)
-            res_count = self._execute_query(data_source_count)
+            res_count = self._execute_query(data_source_count).next()
             res = self._execute_query(data_source)
             total_count = res_count['count'] if res_count['count'] is not None else 0
             df = pd.DataFrame(list(res))
