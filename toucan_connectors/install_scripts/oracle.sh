6be768f4e4a0edbcd5a9fd64528b581e66b0f9d7
Erwan Ben Souiden <erwan@aleikoum.net>
2020-07-21 15:02:42 +0200

    install_scripts :: oracle - still missing unzip package

--- toucan_connectors/install_scripts/oracle.sh
+++ toucan_connectors/install_scripts/oracle.sh
@@ -7,7 +7,7 @@ if [[ -e ~/oracle-installed ]]; then
 fi
 
 apt-get update
-apt-get install -fyq libaio1 curl wget
+apt-get install -fyq libaio1 curl wget unzip
 mkdir -p /opt/oracle
 curl -s 'https://raw.githubusercontent.com/circulosmeos/gdown.pl/master/gdown.pl' -o /tmp/gdown.pl
 chmod +x /tmp/gdown.pl
