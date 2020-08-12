3a0bd01626ea0cff765eea172ca993067861a0c4
Luc Leonard <git@luc-leonard.fr>
2020-08-12 16:41:02 +0200

    first working version

--- tests/github/test_github.py
+++ tests/github/test_github.py
@@ -1,8 +1,33 @@
+import pandas
 import pytest
+from pydantic import ValidationError
 
 from toucan_connectors.github.github_connector import GithubConnector, GithubDataSource
 
 
-def test_get_df():
-    pass
+def test_no_user():
+    """ It should raise an error as no token is given """
+    with pytest.raises(ValidationError):
+        GithubConnector()
 
+
+def test_get_df():
+    pandas.set_option('display.max_colwidth', -1)
+    connector = GithubConnector(name="github", token='b566c86b169ecde44a0304cfe56590d794ef833e')
+    df = connector.get_df(GithubDataSource(name='test', domain='test',
+                                           mapping=[['data', 'repositoryOwner', 'repositories', 'nodes']],
+                                           query='''
+    query{
+        repositoryOwner(login: "toucanToco") {
+            repositories(first:100){
+                nodes{
+                    name,
+                    pullRequests(states:OPEN){
+                        totalCount
+                }
+            }
+        }
+  }
+}'''))
+    # print(df.sort_values(['pullRequests.totalCount'], ascending=False).head(10))
+    assert list(df.columns) == ['name', 'pullRequests.totalCount']
--- toucan_connectors/github/github_connector.py
+++ toucan_connectors/github/github_connector.py
@@ -1,12 +1,22 @@
+import json
+
 import pandas as pd
+import requests
 from pydantic import Field, constr
+from pandas import json_normalize
+from typing import Dict, List, Any
 
 from toucan_connectors.toucan_connector import ToucanConnector, ToucanDataSource
 
 
 class GithubDataSource(ToucanDataSource):
     query: constr(min_length=1) = Field(
-        ..., description='You can write your SQL query here', widget='graphql'
+        ..., description='You can write your graphQL query here', widget='graphql'
+    )
+    mapping: List[List[str]] = Field(
+        [],
+        description="the mapping in the response json. Used to flatten the response. "
+                    "Should point to an array in the json. You can have more than one"
     )
 
 
@@ -14,7 +24,6 @@ class GithubConnector(ToucanConnector):
     data_source_model: GithubDataSource
 
     graphql_endpoint: str = Field(
-        None,
         default='https://api.github.com/graphql',
         description='The graphql endpoint for github',
     )
@@ -24,5 +33,8 @@ class GithubConnector(ToucanConnector):
     )
 
     def _retrieve_data(self, data_source: GithubDataSource) -> pd.DataFrame:
-        pass
+        result = requests.post("https://api.github.com/graphql",
+                               json={"query": data_source.query}, headers={"Authorization": "bearer " + self.token})
+        df = pd.json_normalize(data=result.json(), record_path=data_source.mapping)
+        return df
 
