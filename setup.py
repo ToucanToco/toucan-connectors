80d52c7550bd0c0d888d9ff8666bfabb454e6f28
testinnplayin <archaurore@gmail.com>
2020-09-21 09:59:45 +0200

    v0.41.3

--- setup.py
+++ setup.py
@@ -66,7 +66,7 @@ def get_static_file_paths():
 
 setup(
     name='toucan_connectors',
-    version='0.41.2',
+    version='0.41.3',
     description='Toucan Toco Connectors',
     long_description=(HERE / 'README.md').read_text(encoding='utf-8'),
     long_description_content_type='text/markdown',
