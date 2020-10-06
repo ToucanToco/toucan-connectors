cb3ac774a158fa75e00789d81526667fea9b1cdb
David Nowinsky <david@nowinsky.net>
2020-10-06 10:43:46 +0200

    chore: remove py<3.8 specific code

--- tests/aircall/helpers.py
+++ tests/aircall/helpers.py
@@ -1,13 +1,6 @@
 """Helpers functions for setting up tests for AirCall connector"""
 
-from tests.general_helpers import check_py_version
 from toucan_connectors.aircall.aircall_connector import AircallConnector, AircallDataSource
-
-# we want this module's code to be checked against Python3.8
-# versions prior to Python3.8 don't handle mocks same way for async functions
-PY_VERSION_TO_CHECK = (3, 8)
-
-is_py_version_older = check_py_version(PY_VERSION_TO_CHECK)
 
 
 def build_con_and_ds(dataset: str):
@@ -33,15 +26,9 @@ def build_con_and_ds(dataset: str):
 
 def assert_called_with(fake_func, expected_params=[], expected_count=None):
     """Tests mock function with called/awaited depending on Python version"""
-    if is_py_version_older:
-        if expected_count is not None:
-            assert fake_func.call_count == expected_count
-        if expected_params:
-            fake_func.assert_called_with(*expected_params)
+    if expected_count is not None:
+        assert fake_func.await_count == expected_count
+    if expected_params:
+        fake_func.assert_awaited_with(*expected_params)
     else:
-        if expected_count is not None:
-            assert fake_func.await_count == expected_count
-        if expected_params:
-            fake_func.assert_awaited_with(*expected_params)
-        else:
-            fake_func.assert_awaited_once()
+        fake_func.assert_awaited_once()
