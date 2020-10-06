5aa2e9acda2a43fa4f587d0a0acca19de15fd3c7
Raphaël <raphael.vignes@toucantoco.com>
2020-10-06 09:12:43 +0200

    added jwt in live data mode
    new ROK authentication method
    
    added unit test for live instance with jwt

--- tests/rok/test_rok.py
+++ tests/rok/test_rok.py
@@ -1,6 +1,6 @@
 import pytest
 import responses
-
+from jwt import encode, decode
 from toucan_connectors.rok.rok_connector import RokConnector, RokDataSource
 
 rds = RokDataSource(
@@ -21,6 +21,29 @@ rc = RokConnector(
 endpoint = 'https://rok.example.com/graphql'
 
 
+@pytest.fixture
+def ROK_con():
+    return RokConnector(
+        name='RokConnector',
+        host='https://rok.example.com',
+        username='username',
+        password='password',
+        secret='mylittlesecret',
+    )
+
+
+@pytest.fixture
+def ROK_ds():
+    return RokDataSource(
+        name='RokConnector',
+        domain='RokData',
+        database='database',
+        query='{some query}',
+        filter='.data',
+        live_data=True,
+    )
+
+
 @responses.activate
 def test_rok():
 
@@ -30,6 +53,84 @@ def test_rok():
     df = rc.get_df(rds)
     assert df['a'].sum() == 1
     assert df['b'].sum() == 2
+
+@responses.activate
+def test_rok_with_jwt():
+    """Check that we correctly retrieve the data with the crafted token"""
+    # First query: we receive the ROK token
+    ROK_con = RokConnector(
+    name='RokConnector',
+    host='https://rok.example.com',
+    username='username',
+    password='password',
+    secret='mylittlesecret'
+    )
+
+    ROK_ds = RokDataSource(
+    name='RokConnector',
+    domain='RokData',
+    database='database',
+    query='{some query}',
+    filter='.data',
+    live_data = True
+    )
+
+    data = {'token':encode({'test':'data'}, ROK_con.secret, algorithm='HS256').decode('utf-8')}
+    responses.add(
+        responses.POST, 
+        endpoint,
+        json={'data':data}
+        )
+    # Second query: we receive the data
+    responses.add(
+        responses.POST, 
+        endpoint, 
+        json={'data':{'a':1, 'b':2}}
+        )
+    df = ROK_con.get_df(ROK_ds)
+    assert df['a'].sum() == 1
+    assert df['b'].sum() == 2
+
+
+
+@responses.activate
+def test_rok_with_jwt(ROK_con, ROK_ds):
+    """Check that we correctly retrieve the data with the crafted token"""
+    # First query: we receive the ROK token
+
+    data = {
+        'token': encode({'rok_token': 'rok_token'}, ROK_con.secret, algorithm='HS256').decode(
+            'utf-8'
+        )
+    }
+    responses.add(responses.POST, endpoint, json={'data': data})
+    # Second query: we receive the data
+    responses.add(responses.POST, endpoint, json={'data': {'a': 1, 'b': 2}})
+    df = ROK_con.get_df(ROK_ds)
+    assert df['a'].sum() == 1
+    assert df['b'].sum() == 2
+
+
+def test_live_data_no_secret():
+    """Check that we get an error as the secret is not defined"""
+    ROK_con = RokConnector(
+        name='RokConnector',
+        host='https://rok.example.com',
+        username='username',
+        password='password',
+    )
+
+    ROK_ds = RokDataSource(
+        name='RokConnector',
+        domain='RokData',
+        database='database',
+        query='{some query}',
+        filter='.data',
+        live_data=True,
+    )
+
+    with pytest.raises(ValueError):
+        ROK_con.get_df(ROK_ds)
 
 
 @responses.activate
@@ -51,7 +152,6 @@ def test_rok_data_error():
         rc.get_df(rds)
 
 
-@pytest.mark.skip(reason='This uses a demo api')
 def test_live_instance():
     import os
 
--- toucan_connectors/rok/rok_connector.py
+++ toucan_connectors/rok/rok_connector.py
@@ -1,9 +1,15 @@
+import os
+from datetime import datetime
+
 import pandas as pd
 import requests
+from jwt import decode, encode
 from pydantic import Field
-
+from datetime import datetime
 from toucan_connectors.common import FilterSchema, transform_with_jq
 from toucan_connectors.toucan_connector import ToucanConnector, ToucanDataSource
+from jwt import encode, decode
+import os
 
 
 class RokDataSource(ToucanDataSource):
@@ -14,32 +20,75 @@ class RokDataSource(ToucanDataSource):
 
 class RokConnector(ToucanConnector):
     data_source_model: RokDataSource
-
     host: str
-    username: str
+    username: str 
     password: str
+    secret: str = None
+
 
     def _retrieve_data(self, data_source: RokDataSource) -> pd.DataFrame:
 
-        endpoint = f'{self.host}/graphql?DatabaseName={data_source.database}'
+        endpoint = f'{self.host}/graphql'
 
-        auth_query = """
+        if data_source.live_data:
+            auth_query = """
+            query Auth($database: String!, $token: String!)
+            {authenticateUsingJWT(database: $database, token: $token)}"""
+
+            payload = {
+                'database': data_source.database,
+                'username': self.username,
+                'iat': datetime.utcnow(),
+            }
+
+            if not self.secret:
+                raise ValueError('secret not defined')
+
+
+        if data_source.live_data:
+            auth_query = """
+            query Auth($database: String!, $token: Token!)
+            {authenticateUsingJWT(database: $database, $token: $token)}"""
+
+            payload = {
+            'database':data_source.database,
+            'username':self.username,
+            'iat': datetime.utcnow()
+            }
+
+            encoded_payload = encode(payload, self.secret, algorithm='HS256')
+            auth_vars = {
+                'database': data_source.database,
+                'jwt_token': encoded_payload.decode('utf-8')
+            }
+            encoded_auth_res = requests.post(
+                endpoint, json={'query': auth_query, 'variables': auth_vars}
+            ).json()
+            rok_token = decode(encoded_auth_res['data']['token'], self.secret, algorithm='HS256')
+            payload = {'query': data_source.query, 'variables': data_source.parameters}
+            headers = {'Token': encode(rok_token, self.secret, algorithm='HS256')} #Est-ce que l'on doit le ré encoder ?
+
+        else:
+            auth_query = """
             query Auth($database: String!, $user: String!, $password: String!)
             {authenticate(database: $database, user: $user, password: $password)}"""
-        auth_vars = {
-            'database': data_source.database,
-            'user': self.username,
-            'password': self.password,
-        }
-        auth_res = requests.post(
-            endpoint, json={'query': auth_query, 'variables': auth_vars}
-        ).json()
-        if 'errors' in auth_res:
-            raise ValueError(str(auth_res['errors']))
+            auth_vars = {
+                'database': data_source.database,
+                'user': self.username,
+                'password': self.password,
+            }
+            auth_res = requests.post(
+                endpoint, json={'query': auth_query, 'variables': auth_vars}
+            ).json()
 
-        payload = {'query': data_source.query, 'variables': data_source.parameters}
-        headers = {'Token': auth_res['data']['authenticate']}
+            if 'errors' in auth_res:
+                raise ValueError(str(auth_res['errors']))
+
+            payload = {'query': data_source.query, 'variables': data_source.parameters}
+            headers = {'Token': auth_res['data']['authenticate']}
+
         res = requests.post(endpoint, json=payload, headers=headers).json()
+
         if 'errors' in res:
             raise ValueError(str(res['errors']))
 
