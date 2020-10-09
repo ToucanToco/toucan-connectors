79021512ca2c78288e8fd8e458154a772b66da51
Raphaël <raphael.vignes@toucantoco.com>
2020-10-09 16:18:45 +0200

    Updated helpers.py with good aircall connector import and removing build_future calls
    removed code branch for python version

--- tests/aircall_oauth/helpers.py
+++ tests/aircall_oauth/helpers.py
@@ -1,13 +1,10 @@
 """Helpers functions for setting up tests for AirCall connector"""
 
-from tests.general_helpers import check_py_version
-from toucan_connectors.aircall.aircall_connector import AircallConnector, AircallDataSource
+from toucan_connectors.aircall_oauth.aircall_oauth_connector import (Aircall_oauthConnector,
+Aircall_oauthDataSource)
 
 # we want this module's code to be checked against Python3.8
 # versions prior to Python3.8 don't handle mocks same way for async functions
-PY_VERSION_TO_CHECK = (3, 8)
-
-is_py_version_older = check_py_version(PY_VERSION_TO_CHECK)
 
 
 def build_con_and_ds(dataset: str):
@@ -20,7 +17,13 @@ def build_con_and_ds(dataset: str):
     Replace build_ds function with this one in test_aircall file
     Be sure to also replace the endpoints inside the aircall connector file
     """
-    con = AircallConnector(name='mah_test', bearer_auth_id='abc123efg')
+    con = Aircall_oauthConnector(name='test',
+        auth_flow_id='test',
+        client_id='test_client_id',
+        client_secret='test_client_secret',
+        secrets_keeper=secrets_keeper,
+        redirect_uri='https://redirect.me/')
+
     ds = AircallDataSource(
         name='mah_ds',
         domain='test_domain',
@@ -33,15 +36,9 @@ def build_con_and_ds(dataset: str):
 
 def assert_called_with(fake_func, expected_params=[], expected_count=None):
     """Tests mock function with called/awaited depending on Python version"""
-    if is_py_version_older:
-        if expected_count is not None:
-            assert fake_func.call_count == expected_count
-        if expected_params:
-            fake_func.assert_called_with(*expected_params)
+    if expected_count is not None:
+        assert fake_func.await_count == expected_count
+    if expected_params:
+        fake_func.assert_awaited_with(*expected_params)
     else:
-        if expected_count is not None:
-            assert fake_func.await_count == expected_count
-        if expected_params:
-            fake_func.assert_awaited_with(*expected_params)
-        else:
-            fake_func.assert_awaited_once()
+        fake_func.assert_awaited_once()
