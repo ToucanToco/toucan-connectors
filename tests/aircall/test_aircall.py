1eaa2293a7e96c27d02c45bbce94a37c3fdcf7eb
raphaelvignes <71276735+raphaelvignes@users.noreply.github.com>
2020-10-13 11:12:07 +0200

    feat(Aircall_oAuthConnector): New Aircall connector using oAuthConnector (#226)
    * Quickstarted Aircall Oauth Dance
    
    * added README
    
    * add aircall_oauth new connector
    
    * feat(Aircall_oauthConnector): used oAuth2Connector in Aircall Connector
    
    * Aircall requires client_id & client_secret to be provided in fetch_token method thus we adapted this method in Aircall_oAuthConnector
    
    * Added unit test for specific Aircall retrieval method
    
    * removed import from general_helpers and usage of build_future
    
    * Updated helpers.py with good aircall connector import and removing build_future calls
    
    * removed code branch for python version
    
    * corrected BASE_ROUTE value
    
    * removed unused function on helpers.py
    
    * added doc, commented quickstart script, linted
    
    * renamed file causing tests to fail
    
    * updated test_helpers_aircall_oauth
    
    * Fixed Sonar Issues
    
    In AircallOAuthConnector:
    * a fetch_page with a next page link & a negative limit is tested.
    * NoCrendentialsError error message is a constant
    * The fetch function is tested
    * The uncovered part throwing an HttpError was removed as this case won't exist
    * Created a specific Exception when we get an error from aircall when max number of retries was done
    
    * Replaced the previous Aircall Connector (based on Bearer API) by this one
    
    * Updated connector, test & doc based on comments from review

--- tests/aircall/test_aircall.py
+++ tests/aircall/test_aircall.py
@@ -12,7 +12,15 @@ from tests.aircall.mock_results import (
     more_filtered_teams,
     more_filtered_users,
 )
-from toucan_connectors.aircall.aircall_connector import AircallConnector, AircallDataSource
+from toucan_connectors.aircall.aircall_connector import (
+    AircallConnector,
+    AircallDataSource,
+    NoCredentialsError,
+)
+from toucan_connectors.common import HttpError
+from toucan_connectors.oauth2_connector.oauth2connector import OAuth2Connector
+
+import_path = 'toucan_connectors.aircall.aircall_connector'
 
 columns_for_calls = [
     'id',
@@ -31,11 +39,20 @@ columns_for_teams = ['team', 'user_id', 'user_name', 'user_created_at']
 columns_for_users = ['user_id', 'user_name', 'user_created_at']
 
 fetch_fn_name = 'toucan_connectors.aircall.aircall_connector.fetch_page'
+FAKE_FETCH_RES = 'FAKE RESULTS'
 
 
 @pytest.fixture
-def con(bearer_aircall_auth_id):
-    return AircallConnector(name='test_name', bearer_auth_id=bearer_aircall_auth_id)
+def con(secrets_keeper):
+    secrets_keeper.save('test', {'access_token': 'access_token'})
+    return AircallConnector(
+        name='test',
+        auth_flow_id='test',
+        client_id='test_client_id',
+        client_secret='test_client_secret',
+        secrets_keeper=secrets_keeper,
+        redirect_uri='https://redirect.me/',
+    )
 
 
 def build_ds(dataset: str):
@@ -48,11 +65,35 @@ def build_ds(dataset: str):
     )
 
 
+@pytest.fixture
+def remove_secrets(secrets_keeper, con):
+    secrets_keeper.save('test', {'access_token': None})
+
+
+@pytest.mark.asyncio
+async def test_authentified_fetch(mocker, con):
+    """It should return a result from fetch if all is ok."""
+    mocker.patch(f'{import_path}.fetch', return_value=FAKE_FETCH_RES)
+
+    result = await con._fetch('/foo')
+
+    assert result == FAKE_FETCH_RES
+
+
+def test__run_fetch(mocker, con):
+    """It should return a result from loops if all is ok."""
+    mocker.patch.object(AircallConnector, '_fetch', return_value=FAKE_FETCH_RES)
+    result = con._run_fetch('/foos')
+
+    assert result == FAKE_FETCH_RES
+
+
 @pytest.mark.asyncio
 async def test__get_data_tags_case(con, mocker):
     """Tests with tags happy case"""
     dataset = 'tags'
     fake_res = fake_tags
+    fake_res = fake_res
     fake_fetch_page = mocker.patch(fetch_fn_name, return_value=fake_res)
     ds = build_ds(dataset)
     res = await con._get_tags(ds.dataset, 10)
@@ -72,10 +113,20 @@ async def test__get_data_tags_unhappy_case(con, mocker):
 
 
 @pytest.mark.asyncio
+async def test__get_data_tags_no_secret(con, mocker, remove_secrets):
+    """Checks that we have an exception as we secret was removed"""
+    dataset = 'tags'
+    ds = build_ds(dataset)
+    with pytest.raises(NoCredentialsError):
+        await con._get_tags(ds.dataset, 10)
+
+
+@pytest.mark.asyncio
 async def test__get_data_users_case(con, mocker):
     """Tests users call happy case"""
     dataset = 'users'
     fake_res = [fake_teams, fake_users]
+    fake_res = [item for item in fake_res]
     fake_fetch_page = mocker.patch(fetch_fn_name, side_effect=fake_res)
     ds = build_ds(dataset)
     res = await con._get_data(ds.dataset, 10)
@@ -148,6 +199,7 @@ def test_run_fetches_for_tags(con, mocker):
     """Tests the loop generator function for tags call"""
     dataset = 'tags'
     spy = mocker.spy(AircallConnector, 'run_fetches_for_tags')
+    mocker.patch(f'{import_path}.fetch_page', return_value=fake_tags)
     ds = build_ds(dataset)
     con.run_fetches_for_tags(dataset, ds.limit)
     assert spy.call_count == 1
@@ -157,6 +209,7 @@ def test_run_fetches(con, mocker):
     """Tests the loop generator function for calls/users call"""
     dataset = 'users'
     spy = mocker.spy(AircallConnector, 'run_fetches')
+    mocker.patch(f'{import_path}.fetch_page', return_values=[fake_teams, fake_users])
     ds = build_ds(dataset)
     con.run_fetches(ds.dataset, ds.limit)
     assert spy.call_count == 1
@@ -179,6 +232,7 @@ def test__retrieve_data_no_teams_case(con, mocker):
 def test__retrieve_tags_from_fetch(con, mocker):
     """Tests _retrieve_tags from the fetch_page function"""
     fake_res = fake_tags
+    fake_res = fake_tags
     mocker.patch(fetch_fn_name, return_value=fake_res)
     ds = build_ds('tags')
 
@@ -194,6 +248,7 @@ def test__retrieve_tags_from_fetch(con, mocker):
 def test__retrieve_users_from_fetch(con, mocker):
     """Tests _retrieve_data for users from fetch_page function"""
     fake_res = [fake_teams, fake_users]
+    fake_res = [item for item in fake_res]
     mocker.patch(fetch_fn_name, side_effect=fake_res)
     ds = build_ds('users')
 
@@ -247,3 +302,80 @@ def test_datasource():
         limit=1,
     )
     assert ds.dataset == 'calls'
+
+
+def test_get_status_no_secrets(con, remove_secrets):
+    """
+    It should fail if no secrets are provided
+    """
+    assert con.get_status().status is False
+
+
+def test_get_status_secrets_error(mocker, con):
+    """
+    It should fail if secrets can't be retrieved
+    """
+    mocker.patch(f'{import_path}.OAuth2Connector.get_access_token', side_effect=Exception)
+    assert con.get_status().status is False
+
+
+def test_get_status_api_down(mocker, con):
+    """
+    It should fail if the third-party api is down.
+    """
+    mocker.patch.object(AircallConnector, 'get_access_token', side_effect=HttpError)
+    assert con.get_status().status is False
+
+
+def test_get_status_ok(mocker, con):
+    """
+    Check that we get the connector status set to True if
+    the access token is correctly retrieved
+    """
+    mocker.patch.object(
+        AircallConnector, 'get_access_token', return_value={'access_token': 'access_token'}
+    )
+    assert con.get_status().status is True
+
+
+def test_build_authorization_url(mocker, con):
+    """
+    It should proxy OAuth2Connectors methods
+    """
+    mock_oauth2_connector = mocker.Mock(spec=OAuth2Connector)
+    mock_oauth2_connector.client_id = 'test_client_id'
+    mock_oauth2_connector.client_secret = 'test_client_secret'
+    con.__dict__['_oauth2_connector'] = mock_oauth2_connector
+    con.build_authorization_url()
+    mock_oauth2_connector.build_authorization_url.assert_called()
+
+
+def test_specific_retrieve_token(mocker, con):
+    """Check that the AircallConnector way of retrieving access token works"""
+    mock_oauth2_connector = mocker.Mock(spec=OAuth2Connector)
+    mock_oauth2_connector.client_id = 'test_client_id'
+    mock_oauth2_connector.client_secret = 'test_client_secret'
+    con.__dict__['_oauth2_connector'] = mock_oauth2_connector
+    con.__dict__['_oauth2_connector'].authorization_url = 'https://authorization.url'
+    con.retrieve_tokens('toto')
+    mock_oauth2_connector.retrieve_tokens.assert_called_with(
+        'toto', client_id='test_client_id', client_secret='test_client_secret'
+    )
+
+
+@pytest.mark.asyncio
+async def test__get_data_no_secrets(mocker, con, remove_secrets):
+    """It should raise an exception if there are no secrets returned during the fetch"""
+    dataset = 'users'
+    fake_res = [fake_teams, fake_users]
+    fake_res = [item for item in fake_res]
+    ds = build_ds(dataset)
+    with pytest.raises(NoCredentialsError) as err:
+        await con._get_data(ds.dataset, 10)
+    assert str(err.value) == 'No credentials'
+
+
+def test__run_fetch_no_secret(mocker, con, remove_secrets):
+    """Check that an exception is raised as no access token is available"""
+    with pytest.raises(NoCredentialsError):
+        con._run_fetch('toto')
