b894cba48cdac506f7ecbf718f64d6ce59eebd2c
Luc Leonard <github@luc-leonard.fr>
2020-10-14 12:11:47 +0200

    chores(quickstart): moved aircall quickstart to the quickstart folder

--- quickstart/aircall/quickstart.py
+++ quickstart/aircall/quickstart.py
@@ -2,39 +2,39 @@
 Use this file to initiate the OAuth2 dance and test the connectors that requires it.
 """
 
-from toucan_connectors.google_sheets_2.google_sheets_2_connector import (
-    GoogleSheets2Connector,
-    GoogleSheets2DataSource,
-)
+from helpers import JsonFileSecretsKeeper, get_authorization_response
 
-from .helpers import JsonFileSecretsKeeper, get_authorization_response
+from toucan_connectors.aircall.aircall_connector import (
+    AircallConnector,
+    AircallDataSource,
+)
 
 # Get these info from the provider
 CLIENT_ID = ''
 CLIENT_SECRET = ''
 # ...and give this one to the provider
-REDIRECT_URI = 'http://localhost:34097/'
+REDIRECT_URI = ''
 
-google_sheets_conn = GoogleSheets2Connector(
+aircall_conn = AircallConnector(
     name='test',
     auth_flow_id='test',
     client_id=CLIENT_ID,
     client_secret=CLIENT_SECRET,
     redirect_uri=REDIRECT_URI,
-    secrets_keeper=JsonFileSecretsKeeper(filename='secrets.json'),
+    secrets_keeper=JsonFileSecretsKeeper(filename='secrets_aircall.json'),
 )
-sample_data_source_ss = GoogleSheets2DataSource(
-    name='test',
-    domain='test-connector',
-    spreadsheet_id='1L5YraXEToFv7p0HMke7gXI4IhJotdT0q5bk_PInI1hA',
+sample_data_source_ss = AircallDataSource(
+    name='test', domain='test-connector', limit=10, dataset='calls'
 )
 
 # The OAuth2 authorization process
+# Manually rewrite to localhost:80/....
 authorization_response = get_authorization_response(
-    google_sheets_conn.build_authorization_url(), 'localhost', 34097
+    aircall_conn.build_authorization_url(), 'localhost', 35000
 )
-google_sheets_conn.retrieve_tokens(authorization_response)
+# Here we edited our /etc/hosts file to redirect the uri given by aircall to our localhost
+aircall_conn.retrieve_tokens(authorization_response.replace('http', 'https').replace(':35000', ''))
 
 # The actual data request
-df = google_sheets_conn.get_df(data_source=sample_data_source_ss)
-print(df)
+# Print the retrieved Token, thus ending the oAuth Dance
+print('retrieved aircall token %s' % aircall_conn.get_access_token())
