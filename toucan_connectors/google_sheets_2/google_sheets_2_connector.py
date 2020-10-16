d6b706a77069d58eddaa3fcdeb11ba72b0d7822c
Luc Leonard <git@luc-leonard.fr>
2020-10-16 17:20:43 +0200

    fix(GoogleSheets2): better documentation for the connector configuration

--- toucan_connectors/google_sheets_2/google_sheets_2_connector.py
+++ toucan_connectors/google_sheets_2/google_sheets_2_connector.py
@@ -76,7 +76,17 @@ class GoogleSheets2Connector(ToucanConnector):
 
     @staticmethod
     def get_form():
-        return OauthConnectorConfig.schema()
+        return GoogleSheets2Connector.ConnectorConfig.schema()
+
+    class ConnectorConfig(OauthConnectorConfig):
+        """
+        The connector configuration for Google OAuth.
+        You will need to provide a client_id and a client_secret
+        to get these, you will need to create a new app in
+        the Google Dashboard: https://console.developers.google.com/apis/dashboard.
+        Create a 'Web Application
+        """
+        pass
 
     def __init__(self, **kwargs):
         super().__init__(
