abf554c516677faa9bbf18765dab0462871272a5
Raphaël <raphael.vignes@laposte.net>
2020-10-19 14:16:18 +0200

    Aircall connector's name was changed to Aircall in CONNECTORS_REGISTRY

--- toucan_connectors/__init__.py
+++ toucan_connectors/__init__.py
@@ -12,7 +12,7 @@ CONNECTORS_REGISTRY = {
         'label': 'Adobe Analytics',
         'logo': 'adobe_analytics/adobe-analytics.png',
     },
-    'AircallConnector': {
+    'Aircall': {
         'connector': 'aircall.aircall_connector.AircallConnector',
         'logo': 'aircall/Aircall.svg',
     },
