be9aabe9eaf9913579a62e1b3de1f194f0db4882
raphaelvignes <71276735+raphaelvignes@users.noreply.github.com>
2020-10-22 12:19:36 +0200

    bumps connectors version to 0.43.6 (#239)

--- setup.py
+++ setup.py
@@ -66,7 +66,7 @@ def get_static_file_paths():
 
 setup(
     name='toucan_connectors',
-    version='0.43.5',
+    version='0.43.6',
     description='Toucan Toco Connectors',
     long_description=(HERE / 'README.md').read_text(encoding='utf-8'),
     long_description_content_type='text/markdown',
