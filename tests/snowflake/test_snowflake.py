76c3330843522c8114821922dfd462621e1cca9d
Kaci Adjou <kaci.adjou@toucantoco.com>
2021-01-26 12:27:38 +0100

    snowflake: Add a default warehouse field
    This commit adds a default warehouse field in the Snowflake connector.
    A default warehouse will be applied to any datasource that does not have
    its warehouse field defined.
    
    This commit only provides a default value for the generated form that
    will be displayed on the smallapp, the value has to be submitted from
    the form in the browser.

--- tests/snowflake/test_snowflake.py
+++ tests/snowflake/test_snowflake.py
@@ -3,7 +3,11 @@ import pytest
 from toucan_connectors.snowflake import SnowflakeConnector, SnowflakeDataSource
 
 sc = SnowflakeConnector(
-    name='test_name', user='test_user', password='test_password', account='test_account'
+    name='test_name',
+    user='test_user',
+    password='test_password',
+    account='test_account',
+    default_warehouse='default_wh',
 )
 
 sd = SnowflakeDataSource(
@@ -38,6 +42,43 @@ def test_snowflake(mocker):
     reasq.assert_called_once_with('test_query with bar and pikachu', con=snock())
 
 
+def test_snowflake_data_source_get_form(mocker):
+    ds = SnowflakeDataSource(
+        name='test',
+        domain='blah',
+        database='bleh',
+        query='foo',
+        warehouse='',
+    )
+
+    sf_form = ds.get_form(sc, {})
+
+    assert 'default_wh' == sf_form['properties']['warehouse']['default']
+
+
+def test_snowflake_data_source_default_warehouse(mocker):
+    snow_mock = mocker.patch('snowflake.connector.connect')
+    # Avoid to call read_sql for nothing but no tests required here
+    mocker.patch('pandas.read_sql')
+
+    ds = SnowflakeDataSource(name='test', domain='test', database='db', query='foo', warehouse='')
+
+    sc.get_df(ds)
+
+    snow_mock.return_value.cursor.return_value.execute.assert_called_once_with(
+        'USE WAREHOUSE default_wh'
+    )
+
+    snow_mock.assert_called_once_with(
+        user='test_user',
+        password='test_password',
+        account='test_account',
+        database='db',
+        warehouse='default_wh',
+        ocsp_response_cache_filename=None,
+    )
+
+
 def test_missing_cache_file():
     with pytest.raises(ValueError):
         SnowflakeConnector(
@@ -45,5 +86,10 @@ def test_missing_cache_file():
         )
 
     SnowflakeConnector(
-        user='', password='', account='', name='', ocsp_response_cache_filename=__file__
+        user='',
+        password='',
+        account='',
+        name='',
+        default_warehouse='bleh',
+        ocsp_response_cache_filename=__file__,
     )
