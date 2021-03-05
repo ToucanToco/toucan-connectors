2cd5902a18604274fa5cffaa6d8ea0fc4f7fe0a8
aklom <molka.zaouali@toucantoco.com>
2021-03-05 15:18:44 +0100

    chore: bump to version 0.45.14

--- setup.py
+++ setup.py
@@ -67,7 +67,7 @@ def get_static_file_paths():
 
 setup(
     name='toucan_connectors',
-    version='0.45.13',
+    version='0.45.14',
     description='Toucan Toco Connectors',
     long_description=(HERE / 'README.md').read_text(encoding='utf-8'),
     long_description_content_type='text/markdown',
