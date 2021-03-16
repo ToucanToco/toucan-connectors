33be0bfc16737bee9d20fef22cee1a8dcb732371
David Nowinsky <david@nowinsky.net>
2021-03-16 11:40:06 +0100

    chore: v0.47.0

--- setup.py
+++ setup.py
@@ -67,7 +67,7 @@ def get_static_file_paths():
 
 setup(
     name='toucan_connectors',
-    version='0.46.0',
+    version='0.47.0',
     description='Toucan Toco Connectors',
     long_description=(HERE / 'README.md').read_text(encoding='utf-8'),
     long_description_content_type='text/markdown',
