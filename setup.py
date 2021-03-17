bc04e3a75952b314fd2aa37e84299bd55a48ec55
hachichaud <saucisse.jaune@gmail.com>
2021-03-17 16:30:42 +0100

    chore(version): bump to 0.48.1

--- setup.py
+++ setup.py
@@ -68,7 +68,7 @@ def get_static_file_paths():
 
 setup(
     name='toucan_connectors',
-    version='0.48.0',
+    version='0.48.1',
     description='Toucan Toco Connectors',
     long_description=(HERE / 'README.md').read_text(encoding='utf-8'),
     long_description_content_type='text/markdown',
