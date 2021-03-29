d5a0d27780f9f519521eee50329501f784dadae4
raphael <raphael.vignes@toucantoco.com>
2021-03-29 18:41:59 +0200

    refactor: moved tests on response list content to helpers

--- toucan_connectors/soap/helpers.py
+++ toucan_connectors/soap/helpers.py
@@ -5,10 +5,14 @@ def is_list_response(response):
 def is_nested_list(response):
     # detects if the response is list of list which is a valid response type
     # like [[a, b, c, d]]
-    return len(response) == 1 and isinstance(response[0], list)
+    return len(response) == 1 and isinstance(response[0], list) if len(response[0]) > 0 else False
 
 
 def is_dict_of_lists(response):
     # detects if the response is in the dict of list format
     # e.g [{col1:[value, ...], col2:[value, ...]}]
-    return isinstance(response[0], dict) and isinstance(list(response[0].values())[0], list)
+    return (
+        isinstance(response[0], dict) and isinstance(list(response[0].values())[0], list)
+        if len(response[0]) > 0
+        else False
+    )
