72f06bf18fed8e497c252e92bb2f8eb4f6b571aa
raphael <raphael.vignes@toucantoco.com>
2021-04-26 15:45:54 +0200

    feat: add secrets_storage_version attribute

--- setup.py
+++ setup.py
@@ -70,7 +70,7 @@ def get_static_file_paths():
 
 setup(
     name='toucan_connectors',
-    version='0.51.11',
+    version='0.51.12',
     description='Toucan Toco Connectors',
     long_description=(HERE / 'README.md').read_text(encoding='utf-8'),
     long_description_content_type='text/markdown',
