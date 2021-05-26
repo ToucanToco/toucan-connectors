e147dfcd393d6b387ca3533c63ac583866969e6a
raphael <raphael.vignes@toucantoco.com>
2021-05-26 10:39:41 +0200

    refactor: restore enum labels and translate them for snowflake connect

--- toucan_connectors/snowflake/__init__.py
+++ toucan_connectors/snowflake/__init__.py
@@ -1,1 +1,6 @@
-from .snowflake_connector import AuthenticationMethod, SnowflakeConnector, SnowflakeDataSource
+from .snowflake_connector import (
+    AuthenticationMethod,
+    AuthenticationMethodValue,
+    SnowflakeConnector,
+    SnowflakeDataSource,
+)
