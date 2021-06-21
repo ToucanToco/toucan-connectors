bf274ecf9e6750b9957698c02360732da01bf369
PrettyWood <em.jolibois@gmail.com>
2021-06-21 17:50:47 +0200

    fix: new lib

--- tests/sap_hana/test_sap_hana.py
+++ tests/sap_hana/test_sap_hana.py
@@ -30,12 +30,12 @@ def test_saphana_get_df(mocker):
 
 
 def test_saphana_get_df_no_pw(mocker):
-    snock = mocker.patch('pyhdb.connect')
+    snock = mocker.patch('hdbcli.dbapi.connect')
     reasq = mocker.patch('pandas.read_sql')
 
     saphana_connector = SapHanaConnector(name='test', host='localhost', port=22, user='ubuntu')
     ds = SapHanaDataSource(domain='test', name='test', query='my_query')
     saphana_connector.get_df(ds)
 
-    snock.assert_called_once_with('localhost', 22, 'ubuntu', '')
+    snock.assert_called_once_with(address='localhost', port=22, user='ubuntu', password='')
     reasq.assert_called_once_with('my_query', con=snock(), params=None)
