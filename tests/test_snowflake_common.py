b0825ed479e876d2a9789380d3abcfe827590427
FBB <florent.bertrand@toucantoco.com>
2021-06-30 14:07:49 +0200

    feat(query_pool): resolve test on test_snowflake_common

--- tests/test_snowflake_common.py
+++ tests/test_snowflake_common.py
@@ -2,7 +2,6 @@ import pandas as pd
 import pytest
 import snowflake.connector
 from mock import patch
-from pandas import DataFrame
 
 from toucan_connectors import DataSlice
 from toucan_connectors.snowflake import SnowflakeDataSource
@@ -300,10 +299,8 @@ warehouses_result_one = [{'name': 'warehouse_1'}]
 @patch('snowflake.connector.cursor.SnowflakeCursor.execute', return_value=None)
 @patch('pandas.DataFrame.from_dict', return_value=databases_result_all)
 def test_get_database_without_filter(database_result, execute_query, connect, mocker):
-    spy = mocker.spy(pd.DataFrame, 'from_dict')
     result = SnowflakeCommon().get_databases(connect)
-    # execute_query.call_args_list[0] = 'SHOW DATABASES'
-    assert spy.call_count == 1
+    assert database_result.call_count == 1
     assert result[0] == 'database_1'
     assert result[1] == 'database_2'
     assert len(result) == 2
@@ -313,10 +310,8 @@ def test_get_database_without_filter(database_result, execute_query, connect, mo
 @patch('snowflake.connector.cursor.SnowflakeCursor.execute')
 @patch('pandas.DataFrame.from_dict', return_value=databases_result_none)
 def test_get_database_with_filter_no_result(database_result, execute_query, connect, mocker):
-    spy = mocker.spy(pd.DataFrame, 'from_dict')
     result = SnowflakeCommon().get_databases(connect, 'database_3')
-    # execute_query.call_args_list[0] = 'SHOW DATABASES LIKE database_3'
-    assert spy.call_count == 1
+    assert database_result.call_count == 1
     assert len(result) == 0
 
 
@@ -324,10 +319,8 @@ def test_get_database_with_filter_no_result(database_result, execute_query, conn
 @patch('snowflake.connector.cursor.SnowflakeCursor.execute')
 @patch('pandas.DataFrame.from_dict', return_value=databases_result_one)
 def test_get_database_with_filter_one_result(database_result, execute_query, connect, mocker):
-    spy = mocker.spy(pd.DataFrame, 'from_dict')
     result = SnowflakeCommon().get_databases(connect, 'database_1')
-    # execute_query.call_args_list[0] = 'SHOW DATABASES LIKE database_1'
-    assert spy.call_count == 1
+    assert database_result.call_count == 1
     assert result[0] == 'database_1'
     assert len(result) == 1
 
@@ -336,10 +329,8 @@ def test_get_database_with_filter_one_result(database_result, execute_query, con
 @patch('snowflake.connector.cursor.SnowflakeCursor.execute')
 @patch('pandas.DataFrame.from_dict', return_value=warehouses_result_all)
 def test_get_warehouse_without_filter(warehouse_result, execute_query, connect, mocker):
-    spy = mocker.spy(pd.DataFrame, 'from_dict')
     result = SnowflakeCommon().get_warehouses(connect)
-    # execute_query.call_args_list[0] = 'SHOW WAREHOUSES'
-    assert spy.call_count == 1
+    assert warehouse_result.call_count == 1
     assert result[0] == 'warehouse_1'
     assert result[1] == 'warehouse_2'
     assert len(result) == 2
@@ -349,10 +340,8 @@ def test_get_warehouse_without_filter(warehouse_result, execute_query, connect, 
 @patch('snowflake.connector.cursor.SnowflakeCursor.execute')
 @patch('pandas.DataFrame.from_dict', return_value=warehouses_result_none)
 def test_get_warehouse_with_filter_no_result(warehouse_result, execute_query, connect, mocker):
-    spy = mocker.spy(pd.DataFrame, 'from_dict')
     result = SnowflakeCommon().get_warehouses(connect, 'warehouse_3')
-    # execute_query.call_args_list[0] = 'SHOW WAREHOUSES LIKE warehouse_3'
-    assert spy.call_count == 1
+    assert warehouse_result.call_count == 1
     assert len(result) == 0
 
 
@@ -360,123 +349,112 @@ def test_get_warehouse_with_filter_no_result(warehouse_result, execute_query, co
 @patch('snowflake.connector.cursor.SnowflakeCursor.execute')
 @patch('pandas.DataFrame.from_dict', return_value=warehouses_result_one)
 def test_get_warehouse_with_filter_one_result(warehouse_result, execute_query, connect, mocker):
-    spy = mocker.spy(pd.DataFrame, 'from_dict')
     result = SnowflakeCommon().get_warehouses(connect, 'warehouse_1')
-    # execute_query.call_args_list[0] = 'SHOW WAREHOUSES LIKE warehouse_1'
-    assert spy.call_count == 1
+    assert warehouse_result.call_count == 1
     assert result[0] == 'warehouse_1'
     assert len(result) == 1
 
 
 @patch('snowflake.connector.connect', return_value=snowflake.connector.SnowflakeConnection)
 @patch('snowflake.connector.cursor.SnowflakeCursor.execute')
-@patch('pandas.DataFrame.from_dict', return_value=data_result_all)
+@patch('pandas.DataFrame.from_dict', return_value=pd.DataFrame(data_result_all))
 def test_retrieve_data(result, execute_query, connect, snowflake_datasource, mocker):
-    spy = mocker.spy(pd.DataFrame, 'from_dict')
-    df: DataFrame = SnowflakeCommon().retrieve_data(connect, snowflake_datasource)
-    assert spy.call_count == 1
+    df: pd.DataFrame = SnowflakeCommon().retrieve_data(connect, snowflake_datasource)
+    assert result.call_count == 1
     assert len(df) == 14
 
 
 @patch('snowflake.connector.connect', return_value=snowflake.connector.SnowflakeConnection)
 @patch('snowflake.connector.cursor.SnowflakeCursor.execute')
-@patch('pandas.DataFrame.from_dict', return_value=data_result_all)
+@patch('pandas.DataFrame.from_dict', return_value=pd.DataFrame(data_result_all))
 def test_get_slice_without_limit_without_offset(
     result, execute_query, connect, snowflake_datasource, mocker
 ):
-    spy = mocker.spy(pd.DataFrame, 'from_dict')
     df: DataSlice = SnowflakeCommon().get_slice(connect, snowflake_datasource)
-    assert spy.call_count == 1
+    assert result.call_count == 1
     assert len(df.df) == 14
     assert df.total_count == 14
 
 
 @patch('snowflake.connector.connect', return_value=snowflake.connector.SnowflakeConnection)
 @patch('snowflake.connector.cursor.SnowflakeCursor.execute')
-@patch('pandas.DataFrame.from_dict', return_value=data_result_5)
+@patch('pandas.DataFrame.from_dict', return_value=pd.DataFrame(data_result_5))
 def test_get_slice_with_limit_without_offset(
     result, execute_query, connect, snowflake_datasource, mocker
 ):
-    spy = mocker.spy(pd.DataFrame, 'from_dict')
     df: DataSlice = SnowflakeCommon().get_slice(connect, snowflake_datasource, limit=5)
-    assert spy.call_count == 1
+    assert result.call_count == 1
     assert len(df.df) == 5
     assert df.total_count == 5
 
 
 @patch('snowflake.connector.connect', return_value=snowflake.connector.SnowflakeConnection)
 @patch('snowflake.connector.cursor.SnowflakeCursor.execute')
-@patch('pandas.DataFrame.from_dict', return_value=data_result_none)
+@patch('pandas.DataFrame.from_dict', return_value=pd.DataFrame(data_result_none))
 def test_get_slice_with_limit_without_offset_no_data(
-    result, execute_query, connect, snowflake_datasource, mocker
+    resut, execute_query, connect, snowflake_datasource, mocker
 ):
-    spy = mocker.spy(pd.DataFrame, 'from_dict')
     df: DataSlice = SnowflakeCommon().get_slice(connect, snowflake_datasource, limit=5)
-    assert spy.call_count == 1
+    assert resut.call_count == 1
     assert len(df.df) == 0
     assert df.total_count == 0
 
 
 @patch('snowflake.connector.connect', return_value=snowflake.connector.SnowflakeConnection)
 @patch('snowflake.connector.cursor.SnowflakeCursor.execute')
-@patch('pandas.DataFrame.from_dict', return_value=data_result_one)
+@patch('pandas.DataFrame.from_dict', return_value=pd.DataFrame(data_result_one))
 def test_get_slice_with_limit_without_offset_not_enough_data(
     result, execute_query, connect, snowflake_datasource, mocker
 ):
-    spy = mocker.spy(pd.DataFrame, 'from_dict')
     df: DataSlice = SnowflakeCommon().get_slice(connect, snowflake_datasource, limit=5)
-    assert spy.call_count == 1
+    assert result.call_count == 1
     assert len(df.df) == 1
     assert df.total_count == 1
 
 
 @patch('snowflake.connector.connect', return_value=snowflake.connector.SnowflakeConnection)
 @patch('snowflake.connector.cursor.SnowflakeCursor.execute')
-@patch('pandas.DataFrame.from_dict', return_value=data_result_all)
+@patch('pandas.DataFrame.from_dict', return_value=pd.DataFrame(data_result_all))
 def test_get_slice_with_limit_with_offset(
     result, execute_query, connect, snowflake_datasource, mocker
 ):
-    spy = mocker.spy(pd.DataFrame, 'from_dict')
     df: DataSlice = SnowflakeCommon().get_slice(connect, snowflake_datasource, offset=5, limit=5)
-    assert spy.call_count == 1
+    assert result.call_count == 1
     assert len(df.df) == 5
     assert df.total_count == 5
 
 
 @patch('snowflake.connector.connect', return_value=snowflake.connector.SnowflakeConnection)
 @patch('snowflake.connector.cursor.SnowflakeCursor.execute')
-@patch('pandas.DataFrame.from_dict', return_value=data_result_none)
+@patch('pandas.DataFrame.from_dict', return_value=pd.DataFrame(data_result_none))
 def test_get_slice_with_limit_with_offset_no_data(
     result, execute_query, connect, snowflake_datasource, mocker
 ):
-    spy = mocker.spy(pd.DataFrame, 'from_dict')
     df: DataSlice = SnowflakeCommon().get_slice(connect, snowflake_datasource, offset=5, limit=5)
-    assert spy.call_count == 1
+    assert result.call_count == 1
     assert len(df.df) == 0
     assert df.total_count == 0
 
 
 @patch('snowflake.connector.connect', return_value=snowflake.connector.SnowflakeConnection)
 @patch('snowflake.connector.cursor.SnowflakeCursor.execute')
-@patch('pandas.DataFrame.from_dict', return_value=data_result_one)
+@patch('pandas.DataFrame.from_dict', return_value=pd.DataFrame(data_result_one))
 def test_get_slice_with_limit_with_offset_not_enough_data(
     result, execute_query, connect, snowflake_datasource, mocker
 ):
-    spy = mocker.spy(pd.DataFrame, 'from_dict')
     df: DataSlice = SnowflakeCommon().get_slice(connect, snowflake_datasource, offset=5, limit=5)
-    assert spy.call_count == 1
+    assert result.call_count == 1
     assert len(df.df) == 0
     assert df.total_count == 0
 
 
 @patch('snowflake.connector.connect', return_value=snowflake.connector.SnowflakeConnection)
 @patch('snowflake.connector.cursor.SnowflakeCursor.execute')
-@patch('pandas.DataFrame.from_dict', return_value=data_result_all)
+@patch('pandas.DataFrame.from_dict', return_value=pd.DataFrame(data_result_all))
 def test_get_slice_without_limit_with_offset(
     result, execute_query, connect, snowflake_datasource, mocker
 ):
-    spy = mocker.spy(pd.DataFrame, 'from_dict')
     df: DataSlice = SnowflakeCommon().get_slice(connect, snowflake_datasource, offset=5)
-    assert spy.call_count == 1
+    assert result.call_count == 1
     assert len(df.df) == 14
     assert df.total_count == 14
