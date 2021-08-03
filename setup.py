0e453af7a4a8176e93cc9a8320f9dde2b745242e
FBB <florent.bertrand@toucantoco.com>
2021-08-03 16:36:16 +0200

    chore(bump): v1.2.2

--- setup.py
+++ setup.py
@@ -71,7 +71,7 @@ def get_static_file_paths():
 
 setup(
     name='toucan_connectors',
-    version='1.2.1',
+    version='1.2.2',
     description='Toucan Toco Connectors',
     long_description=(HERE / 'README.md').read_text(encoding='utf-8'),
     long_description_content_type='text/markdown',
