008eaa76c312ce6ada0bc32b8e99696cdbf88483
FBB <florent.bertrand@toucantoco.com>
2021-08-09 15:57:55 +0200

    feat(optimize_connection_manager): fix UT

--- tests/snowflake_oauth2/test_snowflake_oauth2.py
+++ tests/snowflake_oauth2/test_snowflake_oauth2.py
@@ -204,7 +204,7 @@ def test_retrieve_data(
     snowflake_oauth2_datasource,
 ):
     df_result: DataFrame = snowflake_oauth2_connector._retrieve_data(snowflake_oauth2_datasource)
-    assert eq.call_count == 3  # +2 for each data request
+    assert eq.call_count == 2  # +1 select database and warehouse
     assert 11 == len(df_result)
     SnowflakeoAuth2Connector.get_connection_manager().force_clean()
 
@@ -229,7 +229,7 @@ def test_retrieve_data_slice(
     df_result: DataSlice = snowflake_oauth2_connector.get_slice(
         snowflake_oauth2_datasource, offset=0, limit=10
     )
-    assert eq.call_count == 3  # +2 for each data request
+    assert eq.call_count == 2  # +1 select database and warehouse
     assert 11 == len(df_result.df)
     assert 11 == df_result.stats.total_returned_rows
     SnowflakeoAuth2Connector.get_connection_manager().force_clean()
@@ -255,7 +255,7 @@ def test_retrieve_data_slice_with_limit(
     df_result: DataSlice = snowflake_oauth2_connector.get_slice(
         snowflake_oauth2_datasource, offset=5, limit=3
     )
-    assert eq.call_count == 3  # +2 for each data request
+    assert eq.call_count == 2  # +1 select database and warehouse
     assert 11 == len(df_result.df)
     assert 11 == df_result.stats.total_returned_rows
     SnowflakeoAuth2Connector.get_connection_manager().force_clean()
@@ -281,7 +281,7 @@ def test_retrieve_data_slice_too_much(
     df_result: DataSlice = snowflake_oauth2_connector.get_slice(
         snowflake_oauth2_datasource, offset=10, limit=20
     )
-    assert eq.call_count == 3  # +2 for each data request
+    assert eq.call_count == 2  # +1 select database and warehouse
     assert 11 == len(df_result.df)
     assert 11 == df_result.stats.total_returned_rows
     SnowflakeoAuth2Connector.get_connection_manager().force_clean()
