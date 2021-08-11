1e288abb39a1141a74520d00758a823ec0239da0
FBB <82818057+f-bb-toucantoco@users.noreply.github.com>
2021-08-11 09:56:26 +0200

    feat(optimize_connection_manager): check if connection use good database and/or warehouse (#467)
    * feat(optimize_connection_manager): check if connection use good database and/or warehouse
    
    * feat(optimize_connection_manager): fix UT

--- tests/test_snowflake_common.py
+++ tests/test_snowflake_common.py
@@ -140,7 +140,7 @@ def test_get_warehouse_with_filter_one_result(warehouse_result, execute_query, c
 )
 def test_retrieve_data(result, execute_query, connect, snowflake_datasource):
     df: pd.DataFrame = SnowflakeCommon().retrieve_data(connect, snowflake_datasource)
-    assert result.call_count == 3
+    assert result.call_count == 2
     assert len(df) == 14
 
 
@@ -154,7 +154,7 @@ def test_get_slice_without_limit_without_offset(
     result, execute_query, connect, snowflake_datasource
 ):
     ds: DataSlice = SnowflakeCommon().get_slice(connect, snowflake_datasource)
-    assert result.call_count == 3
+    assert result.call_count == 2
     assert len(ds.df) == 14
     assert ds.stats.total_returned_rows == 14
 
@@ -167,7 +167,7 @@ def test_get_slice_without_limit_without_offset(
 )
 def test_get_slice_with_limit_without_offset(result, execute_query, connect, snowflake_datasource):
     ds: DataSlice = SnowflakeCommon().get_slice(connect, snowflake_datasource, limit=5)
-    assert result.call_count == 3
+    assert result.call_count == 2
     assert len(ds.df) == 5
     assert ds.stats.total_returned_rows == 5
 
@@ -182,7 +182,7 @@ def test_get_slice_with_limit_without_offset_no_data(
     result, execute_query, connect, snowflake_datasource
 ):
     ds: DataSlice = SnowflakeCommon().get_slice(connect, snowflake_datasource, limit=5)
-    assert result.call_count == 3
+    assert result.call_count == 2
     assert len(ds.df) == 0
     assert ds.stats.total_returned_rows == 0
 
@@ -197,7 +197,7 @@ def test_get_slice_with_limit_without_offset_not_enough_data(
     result, execute_query, connect, snowflake_datasource
 ):
     ds: DataSlice = SnowflakeCommon().get_slice(connect, snowflake_datasource, limit=5)
-    assert result.call_count == 3
+    assert result.call_count == 2
     assert len(ds.df) == 1
     assert ds.stats.total_returned_rows == 1
 
@@ -210,7 +210,7 @@ def test_get_slice_with_limit_without_offset_not_enough_data(
 )
 def test_get_slice_with_limit_with_offset(result, execute_query, connect, snowflake_datasource):
     ds: DataSlice = SnowflakeCommon().get_slice(connect, snowflake_datasource, offset=5, limit=5)
-    assert result.call_count == 3
+    assert result.call_count == 2
     assert len(ds.df) == 14
     assert ds.stats.total_returned_rows == 14
 
@@ -225,7 +225,7 @@ def test_get_slice_with_limit_with_offset_no_data(
     result, execute_query, connect, snowflake_datasource
 ):
     ds: DataSlice = SnowflakeCommon().get_slice(connect, snowflake_datasource, offset=5, limit=5)
-    assert result.call_count == 3
+    assert result.call_count == 2
     assert len(ds.df) == 0
     assert ds.stats.total_returned_rows == 0
 
@@ -240,7 +240,7 @@ def test_get_slice_with_limit_with_offset_not_enough_data(
     result, execute_query, connect, snowflake_datasource
 ):
     ds: DataSlice = SnowflakeCommon().get_slice(connect, snowflake_datasource, offset=5, limit=5)
-    assert result.call_count == 3
+    assert result.call_count == 2
     assert len(ds.df) == 1
     assert ds.stats.total_returned_rows == 1
 
@@ -253,7 +253,7 @@ def test_get_slice_with_limit_with_offset_not_enough_data(
 )
 def test_get_slice_without_limit_with_offset(result, execute_query, connect, snowflake_datasource):
     ds: DataSlice = SnowflakeCommon().get_slice(connect, snowflake_datasource, offset=5)
-    assert result.call_count == 3
+    assert result.call_count == 2
     assert len(ds.df) == 14
     assert ds.stats.total_returned_rows == 14
 
@@ -283,7 +283,7 @@ def test_get_slice_metadata_no_select_in_query(result, snowflake_datasource, moc
     )
     connect = mocker.MagicMock()
     ds: DataSlice = SnowflakeCommon().get_slice(connect, snowflake_datasource)
-    assert result.call_count == 3
+    assert result.call_count == 2
     assert ds
 
 
@@ -390,7 +390,6 @@ def test__describe_api_didnt_describe(connect, mocker):
     'pandas.DataFrame.from_dict',
     side_effect=[
         None,
-        None,
         pd.DataFrame(data_result_all),
         pd.DataFrame({'TOTAL_ROWS': 20}, index=[0]),
     ],
@@ -400,7 +399,7 @@ def test_retrieve_data_with_row_count_limit_in_query(connect, fetchmany, snowfla
     snowflake_datasource.query = 'select name from favourite_drinks limit 10;'
     sc = SnowflakeCommon()
     sc.retrieve_data(connect, snowflake_datasource, get_row_count=True)
-    assert fetchmany.call_count == 4  # +2 for each data request
+    assert fetchmany.call_count == 3  # +1 to select database and warehouse
     assert sc.total_rows_count == 20
 
 
--- toucan_connectors/snowflake_common.py
+++ toucan_connectors/snowflake_common.py
@@ -5,7 +5,7 @@ from typing import Dict, List, Optional
 
 import pandas as pd
 from pydantic import Field, constr
-from snowflake.connector import DictCursor
+from snowflake.connector import DictCursor, SnowflakeConnection
 
 from toucan_connectors.query_manager import QueryManager
 from toucan_connectors.sql_query_helper import SqlQueryHelper
@@ -81,7 +81,7 @@ class SnowflakeCommon:
 
     def _execute_query_internal(
         self,
-        connection,
+        connection: SnowflakeConnection,
         query: str,
         query_parameters: Optional[dict] = None,
     ) -> pd.DataFrame:
@@ -123,7 +123,7 @@ class SnowflakeCommon:
 
     def _execute_parallelized_queries(
         self,
-        connection,
+        connection: SnowflakeConnection,
         query: str,
         query_parameters: Optional[Dict] = None,
         offset: Optional[int] = None,
@@ -171,14 +171,22 @@ class SnowflakeCommon:
 
     def fetch_data(
         self,
-        connection,
+        connection: SnowflakeConnection,
         data_source: SfDataSource,
         offset: Optional[int] = None,
         limit: Optional[int] = None,
         get_row_count: bool = False,
     ) -> pd.DataFrame:
-        self._execute_query(connection, f'USE DATABASE {data_source.database}')
-        self._execute_query(connection, f'USE WAREHOUSE {data_source.warehouse}')
+        prepare_query: List = []
+        if data_source.database != connection.database:
+            prepare_query.append(f'USE DATABASE {data_source.database}')
+        if data_source.warehouse != connection.warehouse:
+            prepare_query.append(f'USE WAREHOUSE {data_source.warehouse}')
+        if len(prepare_query) > 0:
+            self._execute_query(connection, ';'.join(prepare_query) + ';')
+            self.logger.info(
+                f'Connection changed to use database {connection.database} and warehouse {connection.warehouse}'
+            )
 
         ds = self._execute_parallelized_queries(
             connection, data_source.query, data_source.parameters, offset, limit, get_row_count
@@ -186,13 +194,13 @@ class SnowflakeCommon:
         return ds.df
 
     def retrieve_data(
-        self, c, data_source: SfDataSource, get_row_count: bool = None
+        self, connection: SnowflakeConnection, data_source: SfDataSource, get_row_count: bool = None
     ) -> pd.DataFrame:
-        return self.fetch_data(c, data_source, get_row_count=get_row_count)
+        return self.fetch_data(connection, data_source, get_row_count=get_row_count)
 
     def get_slice(
         self,
-        connection,
+        connection: SnowflakeConnection,
         data_source: SfDataSource,
         offset: Optional[int] = None,
         limit: Optional[int] = None,
@@ -242,7 +250,7 @@ class SnowflakeCommon:
 
     def _describe(
         self,
-        connection,
+        connection: SnowflakeConnection,
         query: str,
     ) -> Dict[str, str]:
         description_start = timer()
