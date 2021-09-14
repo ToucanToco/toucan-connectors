7fa3a5bfd22d835f7f2d42fc162031f91cf6c0da
FBB <florent.bertrand@toucantoco.com>
2021-09-14 16:24:51 +0200

    chore(bump): upgrade to 1.2.9

--- setup.py
+++ setup.py
@@ -71,7 +71,7 @@ def get_static_file_paths():
 
 setup(
     name='toucan_connectors',
-    version='1.2.8',
+    version='1.2.9',
     description='Toucan Toco Connectors',
     long_description=(HERE / 'README.md').read_text(encoding='utf-8'),
     long_description_content_type='text/markdown',
