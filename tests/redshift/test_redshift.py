8f07dbccbfb110140280b629af62ad13a6828dee
Manon GABARRET <manon.gabarret@teamwork.net>
2021-12-13 18:29:43 +0100

    [WIP] tests unitaires in progress

--- tests/redshift/test_redshift.py
+++ tests/redshift/test_redshift.py
@@ -1,6 +1,7 @@
 from unittest.mock import Mock, patch
 
 import pytest
+from redshift_connector.error import InterfaceError, ProgrammingError
 
 from toucan_connectors.redshift.redshift_database_connector import (
     RedshiftConnector,
@@ -25,18 +26,33 @@ def redshift_datasource():
     )
 
 
-# @patch('toucan_connectors.redshift.redshift_database_connector.redshift_connector')
-# def test_redshiftdatasource_get_form(
-#     mock_redshift_connector, redshift_connector, redshift_datasource
-# ):
-#     current_config = {'database': ['table1', 'table2', 'table3']}
-#     mock_redshift_connector.connect().return_value = Mock()
-#     result = redshift_datasource.get_form(redshift_connector, current_config)
-#     print(result)
-#     assert result['properties']['parameters']['title'] == 'Parameters'
-#     assert result['properties']['domain']['title'] == 'Domain'
-#     assert result['properties']['validation']['title'] == 'Validation'
-#     assert result['required'] == ['domain', 'name', 'database', 'query']
+def test_redshiftdatasource_init():
+    ds = RedshiftDataSource(domain='test', name='redshift', database='test', table='table_test')
+    assert ds.query == 'select * from table_test;'
+    assert ds.table == 'table_test'
+
+    with pytest.raises(ValueError):
+        ds = RedshiftDataSource(domain='test', name='redshift', database='test')
+        assert ds.query == None
+        assert ds.table == None
+
+
+@patch.object(RedshiftConnector, '_retrieve_tables')
+def test_redshiftdatasource_get_form(redshift_connector, redshift_datasource):
+    current_config = {'database': ['table1', 'table2', 'table3']}
+    redshift_connector._retrieve_tables.return_value = ['table1', 'table2', 'table3']
+    result = redshift_datasource.get_form(redshift_connector, current_config)
+    assert result['properties']['parameters']['title'] == 'Parameters'
+    assert result['properties']['domain']['title'] == 'Domain'
+    assert result['properties']['validation']['title'] == 'Validation'
+    assert result['required'] == ['domain', 'name', 'database']
+
+
+@patch('toucan_connectors.redshift.redshift_database_connector.redshift_connection_manager')
+def test_redshiftconnector_get_redshift_connection_manager(
+    mock_connection_manager, redshift_connector
+):
+    assert redshift_connector.get_redshift_connection_manager() == mock_connection_manager
 
 
 def test_redshiftconnector_get_connection_params(redshift_connector):
@@ -68,6 +84,13 @@ def test_redshiftconnector_get_cursor(mock_get_connection, redshift_connector, r
 
 
 @patch.object(RedshiftConnector, '_get_cursor')
+def test_redshiftconnector_retrieve_tables(mock_cursor, redshift_connector, redshift_datasource):
+    mock_cursor().__enter__().fetchall.return_value = (['table1'], ['table2'], ['table3'])
+    result = redshift_connector._retrieve_tables(datasource=redshift_datasource)
+    assert result == ['table1', 'table2', 'table3']
+
+
+@patch.object(RedshiftConnector, '_get_cursor')
 def test_redshiftconnector_retrieve_data(mock_cursor, redshift_connector, redshift_datasource):
     mock = Mock()
     mock_cursor().__enter__().fetch_dataframe.return_value = mock
@@ -75,18 +98,56 @@ def test_redshiftconnector_retrieve_data(mock_cursor, redshift_connector, redshi
     assert result == mock
 
 
-# @patch.object(RedshiftConnector, '_get_connection')
-# def test_redshiftconnector_get_status(mock_connection, redshift_connector):
-#     mock_connection().__enter__().return_value = Mock()
-#     result = redshift_connector.get_status()
-#     assert result.status is True
-#     assert result.error is None
-#
-#
-# @patch.object(RedshiftConnector, '_get_connection')
-# def test_redshiftconnector_get_status_with_error(mock_connection, redshift_connector):
-#     mock_connection.side_effect = InterfaceError('error mock')
-#     result = redshift_connector.get_status()
-#     assert type(result.error) == InterfaceError
-#     assert result.status is False
-#     assert str(result.error) == 'error mock'
+def test_redshiftconnector__get_details(redshift_connector):
+    result = redshift_connector._get_details(index=0, status=True)
+    assert result == [('Hostname resolved', True), ('Port opened', False), ('Authenticated', False)]
+
+
+@patch.object(RedshiftConnector, '_build_connection')
+@patch.object(RedshiftConnector, 'check_hostname')
+@patch.object(RedshiftConnector, 'check_port')
+def test_redshiftconnector_get_status_true(
+    mock_check_hostname, mock_check_port, mock_build_connection, redshift_connector
+):
+    mock_check_hostname.return_value = 'hostname_test'
+    mock_check_port.return_value = 'port_test'
+    mock_build_connection().__enter__().return_value = True
+    result = redshift_connector.get_status()
+    assert result.status is True
+    assert result.error == None
+
+
+@patch.object(RedshiftConnector, 'check_hostname')
+def test_redshiftconnector_get_status_with_error_host(mock_hostname, redshift_connector):
+    mock_hostname.side_effect = InterfaceError('error mock')
+    result = redshift_connector.get_status()
+    assert type(result.error) == str
+    assert result.status is False
+    assert str(result.error) == 'error mock'
+
+
+@patch.object(RedshiftConnector, 'check_port')
+def test_redshiftconnector_get_status_with_error_port(mock_hostname, redshift_connector):
+    mock_hostname.side_effect = InterfaceError('error mock')
+    result = redshift_connector.get_status()
+    assert type(result.error) == str
+    assert result.status is False
+    assert str(result.error) == 'error mock'
+
+
+@patch.object(RedshiftConnector, '_build_connection')
+@patch.object(RedshiftConnector, 'check_hostname')
+@patch.object(RedshiftConnector, 'check_port')
+def test_redshiftconnector_get_status_true(
+    mock_check_hostname, mock_check_port, mock_build_connection, redshift_connector
+):
+    mock_check_hostname.return_value = 'hostname_test'
+    mock_check_port.return_value = 'port_test'
+    redshift_connector.user = 'user_test'
+    mock_build_connection.side_effect = ProgrammingError(
+        "'S': 'FATAL', 'C': '3D000', 'M': 'database user_test does not exist'"
+    )
+    result = redshift_connector.get_status()
+    print(result)
+    assert result.status is True
+    assert result.error == "None"
--- toucan_connectors/redshift/redshift_database_connector.py
+++ toucan_connectors/redshift/redshift_database_connector.py
@@ -1,14 +1,19 @@
 import logging
+from contextlib import suppress
+from enum import Enum
+from time import sleep
+from typing import Dict, List, Optional
+from timeit import default_timer as timer
 
 import pandas as pd
 import redshift_connector
-from pydantic import Field, SecretStr
+from pydantic import Field, SecretStr, create_model
 from pydantic.types import constr
 
+from toucan_connectors.common import ConnectorStatus
 from toucan_connectors.connection_manager import ConnectionManager
-from toucan_connectors.toucan_connector import ToucanConnector, ToucanDataSource
+from toucan_connectors.toucan_connector import ToucanConnector, ToucanDataSource, strlist_to_enum
 
-DATABASE_QUERY = """select datname from pg_database;"""
 TABLE_QUERY = """SELECT DISTINCT tablename FROM pg_table_def WHERE schemaname = 'public';"""
 
 logger = logging.getLogger(__name__)
@@ -16,107 +21,179 @@ logger = logging.getLogger(__name__)
 redshift_connection_manager = None
 if not redshift_connection_manager:
     redshift_connection_manager = ConnectionManager(
-        name='redshift', timeout=10, wait=0.2, time_between_clean=10, time_keep_alive=600
+        name='redshift', timeout=10, wait=0.2, time_between_clean=10, time_keep_alive=10
     )
+
+
+class AuthenticationMethod(str, Enum):
+    DB_CREDENTIAL: str = 'db_cred'
+    IAM: str = 'iam'
 
 
 class RedshiftDataSource(ToucanDataSource):
     database: str = Field(..., description='The name of the database you want to query')
-    table: str = Field(None, description='The name of the data table that you want to ')
     query: constr(min_length=1) = Field(
-        description='A string describing a query (CAUTION: Use limit to avoid to retrieve too many datas)',
+        None,
+        description='You can write a custom query against your '
+        'database here. It will take precedence over '
+        'the "table" parameter',
         widget='sql',
     )
+    table: constr(min_length=1) = Field(
+        None,
+        description='The name of the data table that you want to '
+        'get (equivalent to "SELECT * FROM '
+        'your_table")',
+    )
 
-    # @classmethod
-    # def get_form(cls, connector: 'RedshiftConnector', current_config):
-    #     constraints = {}
-    #
-    #     with suppress(Exception):
-    #         connection = redshift_connector.connect(
-    #             **connector.get_connection_params(database=current_config.get('database'))
-    #         )
-    #         with connection.cursor() as cursor:
-    #             cursor.execute(DATABASE_QUERY)
-    #             res = cursor.fetchall()
-    #             available_dbs = [datname for (datname,) in res]
-    #             constraints['database'] = strlist_to_enum('database', available_dbs)
-    #
-    #             if 'database' in current_config:
-    #                 cursor.execute(TABLE_QUERY)
-    #                 res = cursor.fetchall()
-    #                 available_tables = [table_name for (_, table_name) in res]
-    #                 constraints['table'] = strlist_to_enum('table', available_tables, None)
-    #
-    #         return create_model('FormSchema', **constraints, __base__=cls).schema()
+    def __init__(self, **data):
+        super().__init__(**data)
+        query = data.get('query')
+        table = data.get('table')
+        if query is None and table is None:
+            raise ValueError("'query' or 'table' must be set")
+        elif query is None and table is not None:
+            self.query = f'select * from {table};'
+
+    @classmethod
+    def get_form(cls, connector: 'RedshiftConnector', current_config):
+        constraints = {}
+
+        with suppress(Exception):
+            if 'database' in current_config:
+                available_tables = connector._retrieve_tables(current_config['database'])
+                constraints['table'] = strlist_to_enum('table', available_tables, None)
+
+        return create_model('FormSchema', **constraints, __base__=cls).schema()
 
 
 class RedshiftConnector(ToucanConnector):
     data_source_model: RedshiftDataSource
 
+    authentication_method: AuthenticationMethod = Field(
+        AuthenticationMethod.DB_CREDENTIAL,
+        title='Authentication Method',
+        description='The authentication mechanism that will be used to connect to your snowflake data source',
+    )
     user: str = Field(..., description='Your login username.')
     password: SecretStr = Field(None, description='Your login password')
     host: str = Field(None, description='IP address or hostname.')
     port: int = Field(..., description='The listening port of your Redshift Database')
+    connect_timeout: int = Field(
+        None,
+        title='Connection timeout',
+        description='You can set a connection timeout in seconds here, i.e. the maximum length of '
+        'time you want to wait for the server to respond. None by default',
+    )
 
     @staticmethod
-    def get_redshift_connection_manager():
+    def get_redshift_connection_manager() -> ConnectionManager:
         return redshift_connection_manager
 
-    def get_connection_params(self, database):
+    def get_connection_params(self, database) -> Dict:
         con_params = dict(
             database=database,
             user=self.user,
             password=self.password.get_secret_value() if self.password else None,
             host=self.host,
             port=self.port,
+            timeout=self.connect_timeout,
         )
-        # remove None values
         return {k: v for k, v in con_params.items() if v is not None}
 
-    def _get_connection(self, datasource):
+    def _build_connection(self, datasource) -> redshift_connector.Connection:
+        return redshift_connector.connect(
+            **self.get_connection_params(
+                database=datasource.database if datasource is not None else None
+            )
+        )
+
+    def _get_connection(self, datasource) -> redshift_connector.Connection:
         """Establish a connection to an Amazon Redshift cluster."""
 
-        def connect_function():
-            return redshift_connector.connect(
-                **self.get_connection_params(
-                    database=datasource.database if datasource is not None else None
-                )
-            )
+        def connect_function() -> redshift_connector.Connection:
+            return self._build_connection(datasource)
 
-        def alive_function(connection):
-            logger.debug('Check Redshift connection alive')
-            if hasattr(connection, 'is_closed'):
-                print(not connection.is_closed())
-                return not connection.is_closed()
+        def alive_function(connection) -> bool:
+            logger.info(f'Alive Redshift connection: {connection}')
+            if self.connect_timeout is not None:
+                start = timer()
+                while self.connect_timeout - start > 0:
+                    sleep(1)
+                    return True
+            else:
+                return False
 
-        def close_function(connection):
+        def close_function(connection) -> None:
             logger.info('Close Redshift connection')
             if hasattr(connection, 'close'):
                 connection.close()
 
-        connection = redshift_connection_manager.get(
+        connection: RedshiftConnector = redshift_connection_manager.get(
             identifier=datasource.database,
             connect_method=connect_function,
             alive_method=alive_function,
             close_method=close_function,
             save=True if datasource.database else False,
         )
-        return connection.connect_method
+
+        connection.paramstyle = 'pyformat'
+        return connection.__enter__()
 
     def _get_cursor(self, datasource) -> redshift_connector.Cursor:
         return self._get_connection(datasource=datasource).cursor()
 
+    def _retrieve_tables(self, datasource) -> List[str]:
+        with self._get_cursor(datasource=datasource) as cursor:
+            cursor.execute(TABLE_QUERY)
+            res = cursor.fetchall()
+        return [table_name for (table_name,) in res]
+
     def _retrieve_data(self, datasource) -> pd.DataFrame:
         """Get data: tuple from table."""
         with self._get_cursor(datasource=datasource) as cursor:
-            cursor.execute(datasource.query)
+            cursor.execute(datasource.query, datasource.parameters or {})
             result: pd.DataFrame = cursor.fetch_dataframe()
         return result
 
-    # def get_status(self) -> ConnectorStatus:
-    #     try:
-    #         with self._get_connection():
-    #             return ConnectorStatus(status=True, details=None, error=None)
-    #     except InterfaceError as err:
-    #         return ConnectorStatus(status=False, error=err)
+    @staticmethod
+    def _get_details(index: int, status: Optional[bool]):
+        checks = ['Hostname resolved', 'Port opened', 'Authenticated']
+        ok_checks = [(c, True) for i, c in enumerate(checks) if i < index]
+        new_check = (checks[index], status)
+        not_validated_checks = [(c, False) for i, c in enumerate(checks) if i > index]
+        return ok_checks + [new_check] + not_validated_checks
+
+    def get_status(self) -> ConnectorStatus:
+        # Check hostname
+        try:
+            print("try1")
+            self.check_hostname(self.host)
+        except Exception as e:
+            return ConnectorStatus(status=False, details=self._get_details(0, False), error=str(e))
+
+        # Check port
+        try:
+            print("try2")
+            self.check_port(self.host, self.port)
+        except Exception as e:
+            return ConnectorStatus(status=False, details=self._get_details(1, False), error=str(e))
+
+        # Check connection
+        try:
+            print("try3")
+            with self._build_connection(None):
+                return ConnectorStatus(status=True, details=self._get_details(2, True), error=None)
+        except redshift_connector.error.ProgrammingError as ex:
+            print("là")
+            print(ex)
+            # Use to validate if the issue is "only" an issue with database (set after with datasource)
+            if (
+                f"'S': 'FATAL', 'C': '3D000', 'M': 'database \"{self.user}\" does not exist'"
+                in str(ex)
+            ):
+                print("in if")
+                return ConnectorStatus(status=True, details=self._get_details(2, True), error=None)
+        except Exception:
+            print("rr")
+            return ConnectorStatus(status=True, details=self._get_details(2, False), error=None)
