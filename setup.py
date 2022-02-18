b5d076f21c999f076041dd74b07405a0351f880b
raphael.vignes@toucantoco.com <raphael.vignes@toucantoco.com>
2022-02-18 17:38:09 +0100

    chore: bump to 3.1.7

--- setup.py
+++ setup.py
@@ -73,7 +73,7 @@ def get_static_file_paths():
 
 setup(
     name='toucan_connectors',
-    version='3.1.6',
+    version='3.1.7',
     description='Toucan Toco Connectors',
     long_description=(HERE / 'README.md').read_text(encoding='utf-8'),
     long_description_content_type='text/markdown',
