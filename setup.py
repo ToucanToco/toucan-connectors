fc2fbd2b3d8c8b91640e95420fbd7e57a29421dd
austil <augustin@mrlacour.fr>
2022-03-01 14:52:07 +0100

    v3.1.9

--- setup.py
+++ setup.py
@@ -73,7 +73,7 @@ def get_static_file_paths():
 
 setup(
     name='toucan_connectors',
-    version='3.1.8',
+    version='3.1.9',
     description='Toucan Toco Connectors',
     long_description=(HERE / 'README.md').read_text(encoding='utf-8'),
     long_description_content_type='text/markdown',
