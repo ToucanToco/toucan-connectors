2aaa1ceac249186c35873461f78db1e2d1eb61e9
Luka Peschke <luka.peschke@toucantoco.com>
2022-04-07 10:46:06 +0200

    feat(anaplan): Implemented get_form
    Signed-off-by: Luka Peschke <luka.peschke@toucantoco.com>

--- tests/anaplan/test_anaplan.py
+++ tests/anaplan/test_anaplan.py
@@ -1,7 +1,7 @@
 import pytest
 import responses
 
-from toucan_connectors.anaplan.anaplan_connector import AnaplanConnector
+from toucan_connectors.anaplan.anaplan_connector import AnaplanConnector, AnaplanDataSource
 
 
 @pytest.fixture()
@@ -12,9 +12,7 @@ def anaplan_auth_response() -> dict:
 @pytest.fixture()
 def connector() -> AnaplanConnector:
     return AnaplanConnector(
-        username="JohnDoe",
-        password="s3cr3t",
-        name="John's connector",
+        username="JohnDoe", password="s3cr3t", name="John's connector", workspace_id="w1"
     )
 
 
@@ -56,3 +54,56 @@ def test_get_status_expect_auth_failed_invalid_resp_body(connector):
     status = connector.get_status()
     assert not status.status
     assert 'nein' in status.error
+
+
+@responses.activate
+def test_get_form(connector):
+    # response format taken from
+    # https://anaplanbulkapi20.docs.apiary.io/#Models
+    responses.add(
+        responses.POST,
+        'https://auth.anaplan.com/token/authenticate',
+        json={"tokenInfo": {"tokenValue": "youpi"}},
+        status=200,
+    )
+
+    responses.add(
+        responses.GET,
+        'https://api.anaplan.com/2/0/models',
+        status=200,
+        json={
+            # NOTE: There's paging information in here, we
+            # should think about checking if we retrieved all pages
+            "meta": {},
+            # HTTP status info. Are the returned HTTP status
+            # codes consistent with what's in here ?
+            "status": {},
+            "models": [
+                {
+                    "id": "m1",
+                    "activeState": "UNLOCKED",
+                    "name": "ModelOne",
+                    "currentWorkspaceId": "w1",
+                    "currentWorkspaceName": "NiceWorkspace",
+                    "categoryValues": [],
+                }
+            ],
+        },
+    )
+
+    responses.add(
+        responses.GET,
+        'https://api.anaplan.com/2/0/models/m1/views',
+        status=200,
+        json={
+            'views': [
+                {'name': 'ViewOne', 'id': 'm1v1'},
+                {'name': 'ViewTwo', 'id': 'm1v2'},
+            ]
+        },
+    )
+
+    form_schema = AnaplanDataSource.get_form(connector, {'model_id': 'm1'})
+
+    assert form_schema['definitions']['model_id']['enum'] == ['m1']
+    assert form_schema['definitions']['view_id']['enum'] == ['m1v1', 'm1v2']
--- toucan_connectors/anaplan/anaplan_connector.py
+++ toucan_connectors/anaplan/anaplan_connector.py
@@ -1,14 +1,47 @@
+import contextlib
 import json
+from typing import Any, Dict, List
 
 import pandas as pd
 import requests
+from pydantic import Field, constr, create_model
 
 from toucan_connectors.common import ConnectorStatus
-from toucan_connectors.toucan_connector import ToucanConnector, ToucanDataSource
+from toucan_connectors.toucan_connector import ToucanConnector, ToucanDataSource, strlist_to_enum
 
 
 class AnaplanDataSource(ToucanDataSource):
-    query: str
+    model_id: constr(min_length=1) = Field(..., description="The model you want to query")
+    view_id: constr(min_length=1) = Field(..., description="The view you want to query")
+
+    @classmethod
+    def get_form(
+        cls,
+        connector: 'AnaplanConnector',
+        current_config: Dict[str, str],
+    ) -> Dict[str, Any]:
+        """
+        Retrieve a form with suggestions of available Models and Views.
+
+        Once the connector is configured, we can give suggestions for the `model` field.
+        If `model` is set, we can give suggestions for the `view` field.
+        """
+
+        constraints = {}
+        with contextlib.suppress(Exception):  # should we catch AnaplanErrors here instead ?
+            available_models = connector.get_available_models()
+            # TODO: Make it possible to pick models and views by name at some point
+            constraints['model_id'] = strlist_to_enum(
+                'model_id', [m['id'] for m in available_models]
+            )
+
+            if 'model_id' in current_config:
+                available_views = connector.get_available_views(current_config['model_id'])
+                constraints['view_id'] = strlist_to_enum(
+                    'view_id', [v['id'] for v in available_views], default_value=None
+                )
+
+        return create_model('FormSchema', **constraints, __base__=cls).schema()
 
 
 class AnaplanError(Exception):
@@ -21,14 +54,15 @@ class AnaplanAuthError(AnaplanError):
 
 # refactor to fields when required
 _ANAPLAN_AUTH_ROUTE = "https://auth.anaplan.com/token/authenticate"
-_ANAPLAN_API_BASEROUTE = "https://anaplan.com/"
+_ANAPLAN_API_BASEROUTE = "https://api.anaplan.com/2/0"
 
 
 class AnaplanConnector(ToucanConnector):
     data_source_model: AnaplanDataSource
-
     username: str
     password: str
+
+    workspace_id: str = Field(..., description="The ID of the workspace you want to query")
 
     def _retrieve_data(self, data_source: AnaplanDataSource) -> pd.DataFrame:
         raise NotImplementedError
@@ -56,3 +90,23 @@ class AnaplanConnector(ToucanConnector):
             return ConnectorStatus(status=True, message=f"connected as {self.username}")
         except AnaplanAuthError as exc:
             return ConnectorStatus(status=False, error=f"could not retrieve token: {exc}")
+
+    def get_available_models(self) -> List[Dict[str, str]]:
+        token = self._fetch_token()
+        resp = requests.get(
+            f"{_ANAPLAN_API_BASEROUTE}/models",
+            headers={"Accept": "application/json", "Authorization": f"AnaplanAuthToken {token}"},
+        )
+        # TODO: Add same checks as in _fetch_token()
+        body = resp.json()
+        return body['models']
+
+    def get_available_views(self, model_id: str) -> List[Dict[str, str]]:
+        token = self._fetch_token()
+        resp = requests.get(
+            f"{_ANAPLAN_API_BASEROUTE}/models/{model_id}/views",
+            headers={"Accept": "application/json", "Authorization": f"AnaplanAuthToken {token}"},
+        )
+        # TODO: Add same checks as in _fetch_token()
+        body = resp.json()
+        return body['views']
