82680b303427451eb4dabf88a2ee8d22ae869cac
Raphaël VIGNES <raphael.vignes@toucantoco.com>
2022-04-19 17:30:06 +0200

    test: cover databricks connector

--- tests/databricks/test_databricks.py
+++ tests/databricks/test_databricks.py
@@ -1,10 +1,266 @@
+import pyodbc
 import pytest
+from pydantic import ValidationError
+from pytest_mock import MockFixture
 
+from toucan_connectors.common import ConnectorStatus
 from toucan_connectors.databricks.databricks_connector import (
+    DataBricksConnectionError,
     DatabricksConnector,
     DatabricksDataSource,
 )
 
+CONNECTION_STRING = (
+    'Driver=/opt/simba/spark/lib/64/libsparkodbc_sb64.so;'
+    'Host=127.0.0.1;'
+    'Port=443;'
+    'HTTPPath=foo/path;'
+    'ThriftTransport=2;'
+    'SSL=1;'
+    'AuthMech=3;'
+    'UID=token;'
+    'PWD=12345'
+)
 
-def test_get_df():
-    pass
+CONNECTION_STATUS_OK = ConnectorStatus(
+    status=True,
+    error=None,
+    details=[
+        ('Host resolved', True),
+        ('Port opened', True),
+        ('Connected to Databricks', True),
+        ('Authenticated', True),
+    ],
+)
+
+
+def test_postgres_driver_installed():
+    """
+    Check that pgodbc is installed
+    """
+    assert 'Simba Spark ODBC Driver' in pyodbc.drivers()
+
+
+@pytest.fixture
+def databricks_connector() -> DatabricksConnector:
+    return DatabricksConnector(
+        name='test', Host='127.0.0.1', Port='443', HTTPPath='foo/path', PWD='12345'
+    )
+
+
+def test__build_connection_string(databricks_connector: DatabricksConnector):
+    assert databricks_connector._build_connection_string() == CONNECTION_STRING
+
+
+def test_raise_on_empty_query():
+    with pytest.raises(ValidationError):
+        DatabricksDataSource(domain='test', name='test', query='')
+
+
+def test_databricks_get_df(mocker: MockFixture, databricks_connector: DatabricksConnector):
+    mock_pyodbc_connect = mocker.patch('pyodbc.connect')
+    mock_pandas_read_sql = mocker.patch('pandas.read_sql')
+    mocker.patch(
+        'toucan_connectors.databricks.databricks_connector.DatabricksConnector.get_status',
+        return_value=ConnectorStatus(
+            status=True,
+            error=None,
+            details=[
+                ('Host resolved', True),
+                ('Port opened', True),
+                ('Connected to Databricks', True),
+                ('Authenticated', True),
+            ],
+        ),
+    )
+
+    ds = DatabricksDataSource(
+        domain='test',
+        name='test',
+        query='SELECT Name, CountryCode, Population from city LIMIT 2;',
+    )
+    databricks_connector.get_df(ds)
+    mock_pyodbc_connect.assert_called_once_with(CONNECTION_STRING, autocommit=True, ansi=False)
+    mock_pandas_read_sql.assert_called_once_with(
+        'SELECT Name, CountryCode, Population from city LIMIT 2;',
+        con=mock_pyodbc_connect(),
+        params=[],
+    )
+
+
+def test_query_variability(mocker: MockFixture, databricks_connector: DatabricksConnector):
+    """It should connect to the database and retrieve the response to the query"""
+    mock_pyodbc_connect = mocker.patch('pyodbc.connect')
+    mock_pandas_read_sql = mocker.patch('pandas.read_sql')
+    mocker.patch(
+        'toucan_connectors.databricks.databricks_connector.DatabricksConnector.get_status',
+        return_value=ConnectorStatus(
+            status=True,
+            error=None,
+            details=[
+                ('Host resolved', True),
+                ('Port opened', True),
+                ('Connected to Databricks', True),
+                ('Authenticated', True),
+            ],
+        ),
+    )
+
+    ds = DatabricksDataSource(
+        query='select * from test where id_nb > %(id_nb)s and price > %(price)s;',
+        domain='test',
+        name='test',
+        parameters={'price': 10, 'id_nb': 1},
+    )
+
+    databricks_connector.get_df(ds)
+
+    mock_pandas_read_sql.assert_called_once_with(
+        'select * from test where id_nb > ? and price > ?;',
+        con=mock_pyodbc_connect(),
+        params=[1, 10],
+    )
+
+
+def test__retrieve_data_error(databricks_connector: DatabricksConnector) -> None:
+    with pytest.raises(DataBricksConnectionError):
+        databricks_connector._retrieve_data(
+            DatabricksDataSource(
+                domain='test',
+                name='test',
+                query='SELECT Name, CountryCode, Population from city LIMIT 2;',
+            )
+        )
+
+
+def test_get_status_all(databricks_connector: DatabricksConnector, mocker: MockFixture):
+    mocker.patch(
+        'toucan_connectors.databricks.databricks_connector.DatabricksConnector.check_hostname',
+        side_effect=Exception('host unavailable'),
+    )
+    assert databricks_connector.get_status() == ConnectorStatus(
+        status=False,
+        message=None,
+        error='host unavailable',
+        details=[
+            ('Host resolved', False),
+            ('Port opened', None),
+            ('Connected to Databricks', None),
+            ('Authenticated', None),
+        ],
+    )
+    mocker.patch(
+        'toucan_connectors.databricks.databricks_connector.DatabricksConnector.check_port',
+        side_effect=Exception('port closed'),
+    )
+    mocker.patch(
+        'toucan_connectors.databricks.databricks_connector.DatabricksConnector.check_hostname'
+    )
+    assert databricks_connector.get_status() == ConnectorStatus(
+        status=False,
+        message=None,
+        error='port closed',
+        details=[
+            ('Host resolved', True),
+            ('Port opened', False),
+            ('Connected to Databricks', None),
+            ('Authenticated', None),
+        ],
+    )
+    mocker.patch(
+        'pyodbc.connect',
+        side_effect=pyodbc.InterfaceError('Authentication/authorization error occured'),
+    )
+    mocker.patch('toucan_connectors.databricks.databricks_connector.DatabricksConnector.check_port')
+    mocker.patch(
+        'toucan_connectors.databricks.databricks_connector.DatabricksConnector.check_hostname'
+    )
+    assert databricks_connector.get_status() == ConnectorStatus(
+        status=False,
+        message=None,
+        error='Authentication/authorization error occured',
+        details=[
+            ('Host resolved', True),
+            ('Port opened', True),
+            ('Connected to Databricks', True),
+            ('Authenticated', False),
+        ],
+    )
+    mocker.patch(
+        'pyodbc.connect',
+        side_effect=pyodbc.InterfaceError("I don't know mate"),
+    )
+    mocker.patch('toucan_connectors.databricks.databricks_connector.DatabricksConnector.check_port')
+    mocker.patch(
+        'toucan_connectors.databricks.databricks_connector.DatabricksConnector.check_hostname'
+    )
+    assert databricks_connector.get_status() == ConnectorStatus(
+        status=False,
+        message=None,
+        error="I don't know mate",
+        details=[
+            ('Host resolved', True),
+            ('Port opened', True),
+            ('Connected to Databricks', False),
+            ('Authenticated', None),
+        ],
+    )
+    mocker.patch('pyodbc.connect')
+    mocker.patch('toucan_connectors.databricks.databricks_connector.DatabricksConnector.check_port')
+    mocker.patch(
+        'toucan_connectors.databricks.databricks_connector.DatabricksConnector.check_hostname'
+    )
+    assert databricks_connector.get_status() == CONNECTION_STATUS_OK
+
+
+def test__cluster_started_failed(
+    mocker: MockFixture, databricks_connector: DatabricksConnector
+) -> None:
+    mocker.patch(
+        'toucan_connectors.databricks.databricks_connector.DatabricksConnector.get_status',
+        return_value=CONNECTION_STATUS_OK,
+    )
+    mocker.patch('toucan_connectors.databricks.databricks_connector.sleep')
+    connection = mocker.MagicMock()
+    fake_cursor = mocker.MagicMock()
+    connection.cursor.return_value = fake_cursor
+
+    def raise_error() -> None:
+        raise Exception
+
+    fake_cursor.execute = raise_error
+    mocker.patch('pyodbc.connect', return_value=connection)
+    assert databricks_connector._cluster_started() == (False, None)
+
+
+def test__cluster_started_error(
+    mocker: MockFixture, databricks_connector: DatabricksConnector
+) -> None:
+    mocker.patch(
+        'toucan_connectors.databricks.databricks_connector.DatabricksConnector.get_status',
+        return_value=CONNECTION_STATUS_OK,
+    )
+    mocker.patch('pyodbc.connect', side_effect=Exception)
+    with pytest.raises(DataBricksConnectionError):
+        databricks_connector._cluster_started()
+
+
+def test__cluster_started_ok(
+    mocker: MockFixture, databricks_connector: DatabricksConnector
+) -> None:
+    mocker.patch(
+        'toucan_connectors.databricks.databricks_connector.DatabricksConnector.get_status',
+        return_value=CONNECTION_STATUS_OK,
+    )
+    mocker.patch('toucan_connectors.databricks.databricks_connector.sleep')
+    connection = mocker.MagicMock()
+    fake_cursor1 = mocker.MagicMock()
+    fake_cursor2 = mocker.MagicMock()
+    connection.cursor.side_effect = (fake_cursor1, fake_cursor2)
+
+    def raise_error() -> None:
+        raise Exception
+
+    fake_cursor1.execute = raise_error
+    mocker.patch('pyodbc.connect', return_value=connection)
+    assert databricks_connector._cluster_started() == (True, connection)
--- toucan_connectors/databricks/databricks_connector.py
+++ toucan_connectors/databricks/databricks_connector.py
@@ -1,6 +1,5 @@
 from time import sleep
-from typing import Optional
-
+from typing import Any, Optional, Tuple
 
 import pandas as pd
 import pyodbc
@@ -11,22 +10,9 @@ from toucan_connectors.toucan_connector import ToucanConnector, ToucanDataSource
 
 
 class DatabricksDataSource(ToucanDataSource):
-    database: str = Field(
-        None,
-        description='The name of the database you want to query. '
-        "By default SQL Server selects the user's default database",
-    )
-    table: constr(min_length=1) = Field(
-        None,
-        description='The name of the data table that you want to '
-        'get (equivalent to "SELECT * FROM '
-        'your_table")',
-    )
     query: constr(min_length=1) = Field(
         None,
-        description='You can write a custom query against your '
-        'database here. It will take precedence over '
-        'the "table" parameter above',
+        description='You can write a query here',
         widget='sql',
     )
 
@@ -48,17 +34,16 @@ class DatabricksConnector(ToucanConnector):
     HTTPPath: str = Field(
         ..., description='Databricks compute resources URL', placeholder='sql/protocolv1/o/xxx/yyy'
     )
-    UID: str = Field(None, description='User ID if not set value is token')
     PWD: SecretStr = Field(
         ..., description='Your personal access token', placeholder='dapixxxxxxxxxxx'
     )
     Ansi: bool = False
     connect_timeout: int = None
     cluster_start_timeout: int = Field(
-        30, description='The time to wait for cluster to be started before querying'
+        10, description='The time to wait for cluster to be started before querying'
     )
 
-    def _build_connection_string(self, datasource: Optional[DatabricksDataSource] = None) -> str:
+    def _build_connection_string(self) -> str:
         """For constants see: https://docs.databricks.com/integrations/bi/jdbc-odbc-bi.html"""
         connection_params = {
             'Driver': '/opt/simba/spark/lib/64/libsparkodbc_sb64.so',  # This path must be correct when installing
@@ -69,11 +54,8 @@ class DatabricksConnector(ToucanConnector):
             'ThriftTransport': 2,
             'SSL': 1,
             'AuthMech': 3,
-            'UID': self.UID if self.UID else 'token',
-            'PWD': self.PWD,
-            'database': datasource.database if datasource else None,
-            'Autocommit': True,
-            'Ansi': self.Ansi,
+            'UID': 'token',
+            'PWD': self.PWD.get_secret_value(),
             'connect_timeout': self.connect_timeout,
         }
         return ';'.join(f'{k}={v}' for k, v in connection_params.items() if v is not None)
@@ -98,7 +80,7 @@ class DatabricksConnector(ToucanConnector):
         except Exception as e:
             return ConnectorStatus(status=False, details=self._get_details(1, False), error=str(e))
         try:
-            pyodbc.connect(self._build_connection_string())
+            pyodbc.connect(self._build_connection_string(), autocommit=True)
         except (pyodbc.InterfaceError, Exception) as e:
             error_code = e.args[0]
             if 'Authentication/authorization error occured' in error_code:
@@ -111,38 +93,47 @@ class DatabricksConnector(ToucanConnector):
                 )
         return ConnectorStatus(status=True, details=self._get_details(3, True), error=None)
 
-    def _cluster_started(self, datasource: DatabricksDataSource) -> bool:
+    def _cluster_started(self) -> Tuple[bool, Any]:
         """First check that the connection params are valid, then fire a dummy query to start the cluster
         and wait a given time for the answer before returning the check's outcome"""
         checks = self.get_status()
         if checks.status:
             try:
-                with pyodbc.connect(self._build_connection_string(datasource)) as connection:
-                    if connection.cursor.execute('select 1;').fetchone():
-                        return True
-                    else:
-                        sleep(self.cluster_start_timeout)
-                        if connection.cursor.execute('select 1;').fetchone():
-                            return True
-                        else:
-                            return False
+                connection = pyodbc.connect(
+                    self._build_connection_string(), autocommit=True, ansi=self.Ansi
+                )
+                try:
+                    connection.cursor().execute('select 1;').fetchone()
+                    return True, connection
+                except Exception:
+                    sleep(self.cluster_start_timeout)
+                    try:
+                        connection.cursor().execute('select 1;').fetchone()
+                        return True, connection
+                    except Exception:
+                        return False, None
             except Exception:
                 raise DataBricksConnectionError
+        else:
+            return False, None
 
     def _retrieve_data(self, data_source: DatabricksDataSource) -> pd.DataFrame:
         """
         The connector can face a shutdown cluster and must wait it to be started before querying. To do so, call
         _is_cluster_started, then fire the given query
         """
-        with pyodbc.connect(self._build_connection_string(data_source)) as connection:
-            if self._cluster_started(data_source):
-                query_params = data_source.parameters or {}
-                return pandas_read_sql(
-                    data_source.query,
-                    con=connection,
-                    params=query_params,
-                    convert_to_qmark=True,
-                    render_user=True,
-                )
-            else:
-                raise DataBricksConnectionError
+        cluster_started, connection = self._cluster_started()
+
+        if cluster_started:
+            query_params = data_source.parameters or {}
+            result = pandas_read_sql(
+                data_source.query,
+                con=connection,
+                params=query_params,
+                convert_to_qmark=True,
+                render_user=True,
+            )
+            connection.close()
+            return result
+        else:
+            raise DataBricksConnectionError
