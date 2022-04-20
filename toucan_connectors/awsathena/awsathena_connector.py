fe5da98f9f96f01e54fc3f858d2cb7550a6da53a
Luka Peschke <luka.peschke@toucantoco.com>
2022-04-20 15:25:08 +0200

    refactor(awsathena): Simplify config
    Signed-off-by: Luka Peschke <luka.peschke@toucantoco.com>

--- toucan_connectors/awsathena/awsathena_connector.py
+++ toucan_connectors/awsathena/awsathena_connector.py
@@ -1,41 +1,21 @@
 import awswrangler as wr
+import boto3
 import pandas as pd
 from pydantic import Field, constr
-import boto3
 
 from toucan_connectors.toucan_connector import ToucanConnector, ToucanDataSource
 
 
 class AwsathenaDataSource(ToucanDataSource):
     name: str = Field(..., description='Your AWS Athena connector name')
-
-    database: str = Field(
-        None,
-        description='The name of the database you want to query. '
-                    "By default SQL Server selects the user's default database",
-    )
-    table: constr(min_length=1) = Field(
-        None,
-        description='The name of the data table that you want to '
-                    'get (equivalent to "SELECT * FROM '
-                    'your_table")',
+    database: constr(min_length=1) = Field(
+        ..., description='The name of the database you want to query.'
     )
     query: constr(min_length=1) = Field(
-        None,
-        description='You can write a custom query against your '
-                    'database here. It will take precedence over '
-                    'the "table" parameter above',
+        ...,
+        description='The SQL query to execute.',
         widget='sql',
     )
-
-    def __init__(self, **data):
-        super().__init__(**data)
-        query = data.get('query')
-        table = data.get('table')
-        if query is None and table is None:
-            raise ValueError("'query' or 'table' must be set")
-        elif query is None and table is not None:
-            self.query = f'select * from {table};'
 
 
 class AwsathenaConnector(ToucanConnector):
@@ -43,23 +23,23 @@ class AwsathenaConnector(ToucanConnector):
 
     name: str = Field(..., description='Your AWS Athena connector name')
 
-    s3_output_bucket: str = Field(..., description='Your S3 Output bucket')
-    aws_access_key_id: str = Field(..., description='Your AWS access Key')
-    aws_secret_access_key: str = Field(..., description='Your AWS Secret Key')
-    region_name: str = Field(..., description='Your Region Name')
+    s3_output_bucket: str = Field(
+        ..., description='Your S3 Output bucket (where query results are stored.)'
+    )
+    aws_access_key_id: str = Field(..., description='Your AWS access key ID')
+    aws_secret_access_key: str = Field(..., description='Your AWS secret key')
+    region_name: str = Field(..., description='Your AWS region name')
 
     def get_session(self):
         session = boto3.Session(
             aws_access_key_id=self.aws_access_key_id,
             aws_secret_access_key=self.aws_secret_access_key,
-            region_name=self.region_name
+            region_name=self.region_name,
         )
         return session
 
     def _retrieve_data(self, data_source: AwsathenaDataSource) -> pd.DataFrame:
         df = wr.athena.read_sql_query(
-            data_source.query,
-            database=data_source.database,
-            boto3_session=self.get_session()
+            data_source.query, database=data_source.database, boto3_session=self.get_session()
         )
         return df
