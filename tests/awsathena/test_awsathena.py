13b28cdcc795ede0500374db841b59d9b4e79867
Luka Peschke <luka.peschke@toucantoco.com>
2022-04-21 12:01:01 +0200

    feat(awsathena): Add extra unit tests
    Signed-off-by: Luka Peschke <luka.peschke@toucantoco.com>

--- tests/awsathena/test_awsathena.py
+++ tests/awsathena/test_awsathena.py
@@ -1,4 +1,5 @@
 import os
+from typing import Optional
 
 import pandas as pd
 import pytest
@@ -18,41 +19,66 @@ def athena_connector():
     )
 
 
-def test_get_df(mocker, athena_connector):
-    sample_data_source = AwsathenaDataSource(
+@pytest.fixture
+def data_source():
+    return AwsathenaDataSource(
         name='test',
         domain='toto',
         database='mydatabase',
-        query='SELECT * FROM beers',
+        table='beers',
     )
 
+
+@pytest.fixture
+def sample_data() -> pd.DataFrame:
     fixture_path = f'{os.path.dirname(__file__)}/fixtures/beers.csv'
-    fixture_csv = pd.read_csv(fixture_path)
-    read_sql_query_mocked = mocker.patch(
-        'awswrangler.athena.read_sql_query', return_value=fixture_csv
-    )
+    return pd.read_csv(fixture_path)
 
+
+@pytest.fixture
+def mocked_read_sql_query(mocker, sample_data):
+    return mocker.patch('awswrangler.athena.read_sql_query', return_value=sample_data)
+
+
+@pytest.fixture
+def mocked_boto_session(mocker):
     # Mocking because the comparison of two boto3.Session objects is always false
     # We cannot mock get_session on the athena_connector instance directly, because
     # pydantic models alter getattr behaviour
-    boto_session_mocked = mocker.patch.object(
-        AwsathenaConnector, 'get_session', return_value={'a': 'b'}
+    return mocker.patch.object(AwsathenaConnector, 'get_session', return_value={'a': 'b'})
+
+
+def test_AwsathenaDataSource():
+    s1 = AwsathenaDataSource(domain='d', name='source_one', database='db', table='coucou')
+    assert s1.query == 'SELECT * FROM coucou;'
+
+    s2 = AwsathenaDataSource(
+        domain='d', name='source_two', database='db', query='SELECT * FROM coucou;'
     )
+    assert s2.query == 'SELECT * FROM coucou;'
+    assert s2.table is None
 
+    with pytest.raises(ValueError, match="'table' or 'query' must be specified"):
+        AwsathenaDataSource(domain='d', name='source_three', database='db')
+
+
+def test_get_df(
+    mocker, athena_connector, data_source, sample_data, mocked_read_sql_query, mocked_boto_session
+):
     # The actual data request
-    df = athena_connector.get_df(data_source=sample_data_source)
+    df = athena_connector.get_df(data_source=data_source)
 
-    assert df.equals(fixture_csv)
+    assert df.equals(sample_data)
 
-    read_sql_query_mocked.assert_called_once_with(
-        'SELECT * FROM beers',
+    mocked_read_sql_query.assert_called_once_with(
+        'SELECT * FROM beers;',
         database='mydatabase',
         boto3_session={'a': 'b'},
         params={},
         s3_output='s3://test/results/',
     )
 
-    boto_session_mocked.assert_called_once()
+    mocked_boto_session.assert_called_once()
 
 
 def test_get_session(athena_connector):
@@ -61,3 +87,40 @@ def test_get_session(athena_connector):
     creds = sess.get_credentials()
     assert creds.access_key == 'test_access_key_id'
     assert creds.secret_key == 'test_secret_access_key'
+
+
+def test_get_slice(
+    mocker, athena_connector, data_source, mocked_read_sql_query, mocked_boto_session
+):
+    permissions = {'column': 'style', 'operator': 'in', 'value': ['Blonde', 'Triple']}
+    result = athena_connector.get_slice(data_source, permissions=permissions, offset=10, limit=110)
+    assert len(result.df) == result.stats.total_returned_rows == result.stats.total_rows == 4
+    assert sorted(result.df['style'].unique().tolist()) == ['Blonde', 'Triple']
+    mocked_read_sql_query.assert_called_once_with(
+        'SELECT * FROM (SELECT * FROM beers) LIMIT 110 OFFSET 10;',
+        database='mydatabase',
+        boto3_session={'a': 'b'},
+        params={},
+        s3_output='s3://test/results/',
+    )
+
+
+@pytest.mark.parametrize(
+    'query,offset,limit,expected',
+    [
+        # no pagination
+        ('SELECT * FROM toto;', 0, None, 'SELECT * FROM toto;'),
+        # limit only, whitespace, no trailing ;
+        ('   SELECT * FROM toto ', 0, 100, 'SELECT * FROM (SELECT * FROM toto) LIMIT 100;'),
+        # limit only, whitespace, with trailing ;
+        ('  SELECT * FROM toto; ', 0, 100, 'SELECT * FROM (SELECT * FROM toto) LIMIT 100;'),
+        # offset + limit, whitespace, no trailing ;
+        ('  SELECT * FROM toto ', 5, 100, 'SELECT * FROM (SELECT * FROM toto) LIMIT 100 OFFSET 5;'),
+        # offset + limit, whitespace, trailing ;
+        (' SELECT * FROM toto; ', 5, 100, 'SELECT * FROM (SELECT * FROM toto) LIMIT 100 OFFSET 5;'),
+    ],
+)
+def test_add_pagination_to_query(
+    athena_connector, query: str, offset: int, limit: Optional[int], expected: str
+):
+    assert athena_connector._add_pagination_to_query(query, offset=offset, limit=limit) == expected
