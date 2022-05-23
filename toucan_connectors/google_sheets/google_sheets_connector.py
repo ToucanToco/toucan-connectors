19f36e300c979ce3479d4f088ed412025da8c153
Eric Jolibois <em.jolibois@gmail.com>
2022-05-23 19:03:23 +0200

    wip2

--- toucan_connectors/google_sheets/google_sheets_connector.py
+++ toucan_connectors/google_sheets/google_sheets_connector.py
@@ -1,13 +1,25 @@
 from contextlib import suppress
 from datetime import datetime
-from typing import Any, Callable, Dict, List, Optional, Type
+from typing import (
+    TYPE_CHECKING,
+    Any,
+    Callable,
+    ContextManager,
+    Dict,
+    List,
+    Literal,
+    Optional,
+    Type,
+    TypedDict,
+    Union,
+)
 
 import pandas as pd
 from dateutil.relativedelta import relativedelta
 from google.oauth2.credentials import Credentials
 from googleapiclient.discovery import build
 from googleapiclient.errors import Error as GoogleApiClientError
-from pydantic import Field, PrivateAttr, SecretStr, create_model
+from pydantic import Field, PrivateAttr, SecretStr, constr, create_model
 
 from toucan_connectors.common import ConnectorStatus
 from toucan_connectors.toucan_connector import (
@@ -17,6 +29,105 @@ from toucan_connectors.toucan_connector import (
     ToucanDataSource,
     strlist_to_enum,
 )
+
+if TYPE_CHECKING:
+    # Resources
+    Dimension = Literal['ROWS', 'COLUMNS']
+    ValueRenderOption = Literal['FORMATTED_VALUE', 'UNFORMATTED_VALUE', 'FORMULA']
+    DateTimeRenderOption = Literal['SERIAL_NUMBER', 'FORMATTED_STRING']
+    NumberFormatType = Literal[
+        'NUMBER_FORMAT_TYPE_UNSPECIFIED',
+        'TEXT',
+        'NUMBER',
+        'PERCENT',
+        'CURRENCY',
+        'DATE',
+        'TIME',
+        'DATE_TIME',
+        'SCIENTIFIC',
+    ]
+
+    class GridProperties(TypedDict):
+        rowCount: int
+        columnCount: int
+        rowGroupControlAfter: bool
+        columnGroupControlAfter: bool
+
+    class NumberFormat(TypedDict):
+        type: NumberFormatType
+        pattern: str
+
+    class CellFormat(TypedDict):
+        numberFormat: NumberFormat
+
+    class CellData(TypedDict):
+        effectiveFormat: CellFormat
+
+    class RowData(TypedDict):
+        values: List[CellData]
+
+    class GridData(TypedDict):
+        startRow: int
+        startColumn: int
+        rowData: List[RowData]
+
+    SheetType = Literal['GRID', 'OBJECT', 'DATA_SOURCES']
+
+    class SheetProperties(TypedDict):
+        title: str
+        sheetType: SheetType
+        gridProperties: GridProperties
+
+    class Sheet(TypedDict):
+        properties: SheetProperties
+        data: List[GridData]
+
+    class Spreadsheet(TypedDict):
+        sheets: List[Sheet]
+
+    CellValue = Any
+
+    class ValueRange(TypedDict):
+        range: str
+        majorDimension: Dimension
+        values: List[List[CellValue]]
+
+    # Client
+    from typing import Generic, TypeVar
+
+    T = TypeVar('T')
+
+    class ExecuteContext(Generic[T]):
+        def execute(**kwargs: Any) -> T:
+            ...
+
+    class GoogleSheetsClient:
+        class Spreadsheets:
+            def get(
+                *,
+                spreadsheetId: str,
+                ranges: Optional[List[str]] = None,
+                fields: Optional[str] = None,
+            ) -> ExecuteContext[Spreadsheet]:
+                """https://developers.google.com/sheets/api/reference/rest/v4/spreadsheets/get"""
+
+            class SpreadsheetsValues:
+                def get(
+                    *,
+                    spreadsheetId: str,
+                    range: str,
+                    majorDimension: Optional[Dimension] = None,
+                    valueRenderOption: Optional[ValueRenderOption] = None,
+                    dateTimeRenderOption: Optional[DateTimeRenderOption] = None,
+                    fields: Optional[str] = None,
+                ) -> ExecuteContext[ValueRange]:
+                    """https://developers.google.com/sheets/api/reference/rest/v4/spreadsheets.values/get"""
+
+            def values() -> SpreadsheetsValues:
+                ...
+
+        def spreadsheets() -> Spreadsheets:
+            ...
 
 
 class GoogleSheetsDataSource(ToucanDataSource):
@@ -33,8 +144,10 @@ class GoogleSheetsDataSource(ToucanDataSource):
     sheet: Optional[str] = Field(
         None, title='Sheet title', description='Title of the desired sheet'
     )
-    header_row: int = Field(
-        1, title='Header row', description='Row of the header of the spreadsheet'
+    header_range: constr(regex=r'^[A-Z]*\d+:[A-Z]*\d+$') = Field(  # noqa: F722
+        '1:1',
+        title='Header range',
+        description='Range of the header of the spreadsheet (e.g. B1:E1)',
     )
 
     class Config:
@@ -89,7 +202,7 @@ class GoogleSheetsConnector(ToucanConnector):
         # Override it for testing purposes
         return {}
 
-    def build_sheets_api(self):
+    def build_google_sheets_client(self) -> ContextManager['GoogleSheetsClient']:
         return build('sheets', 'v4', cache_discovery=False, **self._google_client_build_kwargs())
 
     def build_oauth2(self):
@@ -99,12 +212,17 @@ class GoogleSheetsConnector(ToucanConnector):
         """
         List available sheets
         """
-        with self.build_sheets_api() as sheets_api:
+        with self.build_google_sheets_client() as client:
             spreadsheet_data = (
-                sheets_api.spreadsheets()
+                client.spreadsheets()
                 .get(
                     spreadsheetId=spreadsheet_id,
-                    fields='sheets.properties.title,sheets.properties.sheetType',
+                    fields=','.join(
+                        (
+                            'sheets.properties.title',
+                            'sheets.properties.sheetType',
+                        )
+                    ),
                 )
                 .execute(**self._google_client_request_kwargs())
             )
@@ -155,26 +273,32 @@ class GoogleSheetsConnector(ToucanConnector):
             sheet_names = self.list_sheets(data_source.spreadsheet_id)
             data_source.sheet = sheet_names[0]
 
-        with self.build_sheets_api() as sheets_api:
-            # Fetch metadata of sheet (see ./__fixtures__/spreadsheet_metadata.json for an example)
+        with self.build_google_sheets_client() as client:
             spreadsheet_metadata = (
-                sheets_api.spreadsheets()
+                client.spreadsheets()
                 .get(
                     spreadsheetId=data_source.spreadsheet_id,
                     # use `repr()` to add single quotes around the name and escape correctly
                     ranges=[repr(data_source.sheet)],
+                    fields=','.join(
+                        (
+                            'sheets.properties.gridProperties',
+                            'sheets.data.rowData.values.effectiveFormat.numberFormat',
+                        )
+                    ),
                 )
                 .execute(**self._google_client_request_kwargs())
             )
-            sheet_props = spreadsheet_metadata['sheets'][0]['properties']['gridProperties']
-            total_rows = sheet_props['rowCount']
+            total_rows = spreadsheet_metadata['sheets'][0]['properties']['gridProperties'][
+                'rowCount'
+            ]
 
             header_data = (
-                sheets_api.spreadsheets()
+                client.spreadsheets()
                 .values()
                 .get(
                     spreadsheetId=data_source.spreadsheet_id,
-                    range=f"{repr(data_source.sheet)}!{data_source.header_row}:{data_source.header_row}",
+                    range=f'{repr(data_source.sheet)}!{data_source.header_range}',
                     dateTimeRenderOption='SERIAL_NUMBER',
                     majorDimension='ROWS',
                     valueRenderOption='UNFORMATTED_VALUE',
@@ -182,7 +306,19 @@ class GoogleSheetsConnector(ToucanConnector):
                 .execute(**self._google_client_request_kwargs())
             )
 
-            start_index = data_source.header_row + 1 + offset
+            try:
+                import re
+
+                range_regex = r'([A-Z]*)(\d+):([A-Z]*)(\d+)'
+                header_col_1, header_row_1_str, header_col_2, header_row_2_str = re.search(
+                    range_regex, data_source.header_range
+                ).groups()
+                assert header_row_1_str == header_row_2_str
+                header_row = int(header_row_1_str)
+            except (AssertionError, TypeError):
+                raise Exception('header range is not valid')
+
+            start_index = header_row + 1 + offset
 
             if limit is None:
                 end_index = total_rows
@@ -192,11 +328,11 @@ class GoogleSheetsConnector(ToucanConnector):
                     end_index = total_rows
 
             slice_data = (
-                sheets_api.spreadsheets()
+                client.spreadsheets()
                 .values()
                 .get(
                     spreadsheetId=data_source.spreadsheet_id,
-                    range=f"{repr(data_source.sheet)}!{start_index}:{end_index}",
+                    range=f'{repr(data_source.sheet)}!{header_col_1}{start_index}:{header_col_2}{end_index}',
                     dateTimeRenderOption='SERIAL_NUMBER',
                     majorDimension='ROWS',
                     valueRenderOption='UNFORMATTED_VALUE',
@@ -204,13 +340,45 @@ class GoogleSheetsConnector(ToucanConnector):
                 .execute(**self._google_client_request_kwargs())
             )
 
-        # header can have more columns than the slice if columns are empty
-        slice_values = slice_data['values']
-        header_cols = header_data['values'][0]
-        len_header_cols = len(header_cols)
+        try:
+            slice_values = slice_data['values']
+        except KeyError:
+            slice_values = []
 
+        try:
+            header_cols = header_data['values'][0]
+        except KeyError:
+            header_cols = []
+
+        # header can have more columns than the slice if columns are empty
+        len_header_cols = len(header_cols)
         for slice_row in slice_values:
             slice_row.extend([None] * (len_header_cols - len(slice_row)))
+
+        # we only modify all the retrieved data if we have some formatting metadata on gsheet
+        try:
+            row_data = spreadsheet_metadata['sheets'][0]['data'][0]['rowData']
+        except (KeyError, IndexError):
+            pass
+        else:
+            for row_idx, row in enumerate(row_data):
+                try:
+                    row_cells = row['values']
+                except KeyError:
+                    continue
+                else:
+                    for col_idx, cell_data in enumerate(row_cells):
+                        try:
+                            cell_format = cell_data['effectiveFormat']
+                        except KeyError:
+                            continue
+                        else:
+                            try:
+                                slice_values[row_idx][col_idx] = format_cell_value(
+                                    slice_values[row_idx][col_idx], cell_format
+                                )
+                            except IndexError:
+                                continue
 
         df = pd.DataFrame(columns=header_cols, data=slice_values)
 
@@ -224,64 +392,13 @@ class GoogleSheetsConnector(ToucanConnector):
         )
 
     def _retrieve_data(self, data_source: GoogleSheetsDataSource) -> pd.DataFrame:
-
-        if data_source.sheet is None:
-            # Select the first sheet by default
-            sheet_names = self.list_sheets(data_source.spreadsheet_id)
-            data_source.sheet = sheet_names[0]
-
-        with self.build_sheets_api() as sheets_api:
-            sheet_data = (
-                sheets_api.spreadsheets()
-                .values()
-                .get(
-                    spreadsheetId=data_source.spreadsheet_id,
-                    range=repr(data_source.sheet),
-                    dateTimeRenderOption='SERIAL_NUMBER',
-                    majorDimension='ROWS',
-                    valueRenderOption='UNFORMATTED_VALUE',
-                )
-                .execute(**self._google_client_request_kwargs())
-            )
-
-            # Fetch metadata associated with values
-            sheet_cell_formats = (
-                sheets_api.spreadsheets()
-                .get(
-                    spreadsheetId=data_source.spreadsheet_id,
-                    fields='sheets.data.rowData.values.effectiveFormat.numberFormat',
-                    ranges=[sheet_data['range']],
-                )
-                .execute(**self._google_client_request_kwargs())
-            )
-
-        def cell_format(row_index: int, column_index: int):
-            try:
-                return sheet_cell_formats['sheets'][0]['data'][0]['rowData'][row_index]['values'][
-                    column_index
-                ]['effectiveFormat']
-            except (KeyError, IndexError):
-                return None
-
-        values = [
-            [
-                parse_cell_value(cell_value, cell_format(row_index, column_index))
-                for column_index, cell_value in enumerate(row_values)
-            ]
-            for row_index, row_values in enumerate(sheet_data['values'])
-        ]
-
-        header_row_index = data_source.header_row - 1
-        df = pd.DataFrame(columns=values[header_row_index], data=values[header_row_index + 1 :])
-
-        # TODO Columns must be uniquely named (raise an error or suffix some of them) - otherwise, .to_json will fail
-        return df
+        return self.get_slice(data_source).df
 
 
 SERIAL_REFERENCE_DAY = datetime.fromisoformat('1899-12-30')
 
 
-def serial_number_to_date(serial_number: float) -> datetime:
+def serial_number_to_date(serial_number: Union[int, float]) -> datetime:
     """
     https://developers.google.com/sheets/api/reference/rest/v4/DateTimeRenderOption
     """
@@ -289,17 +406,15 @@ def serial_number_to_date(serial_number: float) -> datetime:
     return SERIAL_REFERENCE_DAY + relativedelta(days=int(serial_number))
 
 
-def parse_cell_value(value, format):
+def format_cell_value(value: Any, format: Optional['CellFormat']) -> Any:
     """
     Use the format (if provided) to parse the value in its intended type
     """
-    if (
-        (type(value) == int or type(value) == float)
-        and format
-        and 'numberFormat' in format
-        and format['numberFormat']['type'] == 'DATE'
-    ):
-        return serial_number_to_date(value)
+    with suppress(KeyError):
+        number_format_type: 'NumberFormatType' = format['numberFormat']['type']
+        if number_format_type == 'DATE' and isinstance(value, (int, float)):
+            return serial_number_to_date(value)
+
     return value
 
 
