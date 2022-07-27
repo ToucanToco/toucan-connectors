0756271b62c491b78410eab5d6f02891adfd48fb
Luka Peschke <luka.peschke@toucantoco.com>
2022-07-27 14:50:43 +0200

    fix: Ensure STL dates are returned with the right dtype
    BigQuery and Athena translators store objects in the SQL DATE type as datetime.date objects.
    That is correct, but pandas does not consider datetime.date objects as datetimes. As a result, when
    serializing and deserializing a dataframe containing datetime.date objects, these get dumped/loaded
    as strings.
    
    This change ensures the right type is used.
    
    TCTC-3424
    
    Signed-off-by: Luka Peschke <luka.peschke@toucantoco.com>

---
+++ toucan_connectors/utils/datetime.py
@@ -0,0 +1,17 @@
+from datetime import date
+
+import pandas as pd
+from pandas.api.types import is_datetime64_any_dtype as pd_is_datetime
+
+
+def is_datetime_col(col: pd.Series) -> bool:
+    return pd_is_datetime(col) or all(isinstance(val, date) for val in col)
+
+
+def sanitize_df_dates(df: pd.DataFrame) -> pd.DataFrame:
+    """Converts all datetime columns to pd.datetime64"""
+    for col in df.columns:
+        if is_datetime_col(df[col]):
+            df[col] = pd.to_datetime(df[col])
+
+    return df
