46ef8d58b581493f7b168dc747c2cbfa1575e862
Luka Peschke <luka.peschke@toucantoco.com>
2022-08-23 13:57:30 +0200

    fix(conditions): Unquote strings in gt, lt, gte and lte conditions (#650)
    TCTC-3257
    
    Signed-off-by: Luka Peschke <luka.peschke@toucantoco.com>

--- toucan_connectors/pandas_translator.py
+++ toucan_connectors/pandas_translator.py
@@ -1,6 +1,20 @@
-from typing import List
+from ast import literal_eval
+from functools import wraps
+from typing import Callable, TypeAlias
 
-from toucan_connectors.condition_translator import ConditionTranslator
+from toucan_connectors.condition_translator import ConditionTranslator, Number
+
+FnTakingNumber: TypeAlias = Callable[[type['PandasConditionTranslator'], str, Number], str]
+
+
+def requires_number(fn: Callable[[type, str, Number | str], str]) -> FnTakingNumber:
+    @wraps(fn)
+    def wrapper(cls, column, value) -> str:
+        # NOTE: We're only literal-evaling here (unquoting) rather than casting to a float because
+        # we might want to apply these to dates
+        return fn(cls, column, literal_eval(value) if "'" in value or '"' in value else value)
+
+    return wrapper
 
 
 class PandasConditionTranslator(ConditionTranslator):
@@ -21,37 +35,43 @@ class PandasConditionTranslator(ConditionTranslator):
         return f"'{value}'"
 
     @classmethod
-    def join_clauses(cls, clauses: List[str], logical_operator: str) -> str:
+    def join_clauses(cls, clauses: list[str], logical_operator: str) -> str:
         return '(' + f' {logical_operator} '.join(clauses) + ')'
 
     @classmethod
-    def EQUAL(cls, column, value) -> str:
+    def EQUAL(cls, column: str, value: str | Number) -> str:
         return f'{column} == {value}'
 
     @classmethod
-    def NOT_EQUAL(cls, column, value) -> str:
+    def NOT_EQUAL(cls, column: str, value: str | Number) -> str:
         return f'{column} != {value}'
 
     @classmethod
-    def LOWER_THAN(cls, column, value) -> str:
+    @requires_number
+    def LOWER_THAN(cls, column: str, value: Number) -> str:
         return f'{column} < {value}'
 
     @classmethod
-    def LOWER_THAN_EQUAL(cls, column, value) -> str:
+    @requires_number
+    def LOWER_THAN_EQUAL(cls, column: str, value: Number) -> str:
         return f'{column} <= {value}'
 
     @classmethod
-    def GREATER_THAN(cls, column, value) -> str:
+    @requires_number
+    def GREATER_THAN(cls, column: str, value: Number) -> str:
         return f'{column} > {value}'
 
     @classmethod
-    def GREATER_THAN_EQUAL(cls, column, value) -> str:
+    @requires_number
+    def GREATER_THAN_EQUAL(cls, column: str, value: Number) -> str:
         return f'{column} >= {value}'
 
+    # NOTE: Here, we cannot ensure that the parameter has the expected type. Some frontend dev has
+    # to be expected
     @classmethod
-    def IN(cls, column, value) -> str:
+    def IN(cls, column: str, value: str | Number | list[str | Number]) -> str:
         return f'{column} in {value}'
 
     @classmethod
-    def NOT_IN(cls, column, value) -> str:
+    def NOT_IN(cls, column: str, value: str | Number | list[str | Number]) -> str:
         return f'{column} not in {value}'
