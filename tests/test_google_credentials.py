976a055441538a13859db7789179cfd4f0f2bcdc
Eric Jolibois <em.jolibois@gmail.com>
2023-10-18 18:42:56 +0200

    feat(google credentials): `private_key_id` and `private_key` are secrets

--- tests/test_google_credentials.py
+++ tests/test_google_credentials.py
@@ -1,3 +1,5 @@
+import json
+
 from pytest_mock import MockFixture
 
 from toucan_connectors.google_credentials import GoogleCredentials, get_google_oauth2_credentials
@@ -17,6 +19,14 @@ def test_google_credentials(mocker: MockFixture):
         'client_x509_cert_url': 'https://www.googleapis.com/robot/v1/metadata/x509/xxx.iam.gserviceaccount.com',  # noqa: E501
     }
     credentials = GoogleCredentials(**conf)
+    # Ensure `private_key_id` and `private_key` are masked
+    assert credentials.json() == json.dumps(
+        {
+            **conf,
+            'private_key_id': '**********',
+            'private_key': '**********',
+        }
+    )
     # Ensure `Credentials` is called with the right values of secrets
     mock_credentials = mocker.patch('toucan_connectors.google_credentials.Credentials')
     get_google_oauth2_credentials(credentials)
@@ -38,7 +48,7 @@ def test_unespace_break_lines():
     }
     credentials = GoogleCredentials(**conf)
     assert (
-        credentials.private_key == '-----BEGIN PRIVATE KEY-----\n'
+        credentials.private_key.get_secret_value() == '-----BEGIN PRIVATE KEY-----\n'
         'aaa\n'
         'bbb\n'
         '-----END PRIVATE KEY-----\n'
